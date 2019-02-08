4261fd12319f9bb61289bf14cd506c2842c77b03
Danang Argasuma <danang.argasuma@ad-ins.com>
2019-02-08 10:29:59 +0700

    penambahan bas-class dan update adinshttpservice

--- src/app/ad-ins-http-service.service.ts
+++ src/app/ad-ins-http-service.service.ts
@@ -1,6 +1,9 @@
+import { value } from './shared/data/dropdowns';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import { BaseClass } from './base/base-class';
+
 export declare type HttpObserve = 'body' | 'events' | 'response';
 
 @Injectable({
@@ -10,6 +13,7 @@ export class AdInsHttpServiceService {
 
   ipAddress: any;
   loc: string[];
+  baseClass: BaseClass;
 
   constructor(private http: HttpClient,
     private datepipe: DatePipe) {
@@ -18,22 +22,17 @@ export class AdInsHttpServiceService {
 
    createHeader(): HttpHeaders {
     // var displayDate = new Date().toLocaleDateString();
-    this.loc = this.ipAddress.loc.split(',');
+
     const httpHeaders = new HttpHeaders({
-      'Authorization' : 'my-token' ,
+      'Authorization' : 'my-token',
       'Content-Type' : 'application/json',
-      'Cache-Control': 'no-cache',
-      'Ip' : this.ipAddress.ip,
-      'City' : this.ipAddress.city,
-      'Latitude' : this.loc[0],
-      'Longitude' : this.loc[1],
-      'UserName' : 'user1',
-      'SendDateTime' : new Date().toLocaleDateString()
+      'Cache-Control': 'no-cache'
     });
     return httpHeaders;
   }
 
   post(url, data?) {
+    this.baseClass = this.generateBaseClass();
     return this.http.post(url, data, {
       headers: this.createHeader()
     });
@@ -42,5 +41,24 @@ export class AdInsHttpServiceService {
  getIpAddress(): any {
     return this.ipAddress = this.http.get<{ip: string}>('https://ipinfo.io/json')
       .subscribe( data => {});
+
     }
+
+generateBaseClass(data?): BaseClass {
+  this.loc = this.ipAddress.loc.split(',');
+  const getObject = JSON.parse(localStorage.getItem('currentUserContext'));
+  const getFormAccess = JSON.parse(localStorage.getItem('pageAccess'));
+
+  return  this.baseClass = {
+  Ip : this.ipAddress.ip,
+  Latitude : this.loc[0],
+  Longitude : this.loc[1],
+  UserName : getObject.value['user'],
+  Role : getObject.value['role'],
+  Office: getObject.value['office'],
+  LastPageAccess: getFormAccess.value['currUrl'],
+  LastPageAccessTime: getFormAccess.value['bussinessDt'],
+  RequestObject: data
+  }
+}
 }
