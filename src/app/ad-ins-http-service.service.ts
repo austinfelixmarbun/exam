a1c496be4618687f4041fa1addc1681226965729
Steven Febrian Harijanto <steven.fh@ad-ins.com>
2019-02-08 11:36:02 +0700

    rapihin punya Danang

--- src/app/ad-ins-http-service.service.ts
+++ src/app/ad-ins-http-service.service.ts
@@ -1,8 +1,7 @@
-import { value } from './shared/data/dropdowns';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams, HttpHandler, HttpRequest, HttpEvent, HttpResponse } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
-import { BaseClass } from './base/base-class';
+import { BaseObj } from '../app/shared/model/BaseObj.model';
 
 export declare type HttpObserve = 'body' | 'events' | 'response';
 
@@ -13,29 +12,45 @@ export class AdInsHttpServiceService {
 
   ipAddress: any;
   loc: string[];
-  baseClass: BaseClass;
+  baseClass: BaseObj;
 
   constructor(private http: HttpClient,
     private datepipe: DatePipe) {
     //this.getIpAddress();
   }
 
-   createHeader(): HttpHeaders {
-    // var displayDate = new Date().toLocaleDateString();
-
-    const httpHeaders = new HttpHeaders({
-      'Authorization' : 'my-token',
-      'Content-Type' : 'application/json',
-      'Cache-Control': 'no-cache'
-    });
-    return httpHeaders;
+  private generateHeader():any{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token',
+        'Access-Control-Allow-Origin':'true'
+      })
+    };
+    return httpOptions;
   }
 
-  post(url, data?) {
+  getData(url:string,withHeader:boolean=true){
+    const httpOptions = this.generateHeader();
+    if(withHeader==true)
+    {
+      return this.http.get<any>(url,httpOptions);
+    }
+    return this.http.get<any>(url);
+  }
+
+  post(url, data?,withHeader:boolean=true) {
     this.baseClass = this.generateBaseClass();
-    return this.http.post(url, data, {
-      headers: this.createHeader()
-    });
+    this.baseClass.RequestObject = data;
+    const httpOptions = this.generateHeader();
+    if(withHeader==true)
+    {
+      return this.http.post(url, data,httpOptions);
+    }
+    else{
+      return this.http.post(url, data);
+    }
+    
   }
 
  getIpAddress(): any {
@@ -44,21 +59,23 @@ export class AdInsHttpServiceService {
 
     }
 
-generateBaseClass(data?): BaseClass {
+generateBaseClass(data?): BaseObj {
   this.loc = this.ipAddress.loc.split(',');
   const getObject = JSON.parse(localStorage.getItem('currentUserContext'));
   const getFormAccess = JSON.parse(localStorage.getItem('pageAccess'));
+  const localIp = localStorage.getItem("LocalIp");
 
   return  this.baseClass = {
-  Ip : this.ipAddress.ip,
-  Latitude : this.loc[0],
-  Longitude : this.loc[1],
-  UserName : getObject.value['user'],
-  Role : getObject.value['role'],
-  Office: getObject.value['office'],
-  LastPageAccess: getFormAccess.value['currUrl'],
-  LastPageAccessTime: getFormAccess.value['bussinessDt'],
-  RequestObject: data
+    Ip : this.ipAddress.ip,
+    Latitude : this.loc[0],
+    Longitude : this.loc[1],
+    UserName : getObject.value['user'],
+    Role : getObject.value['role'],
+    Office: getObject.value['office'],
+    LastPageAccess: getFormAccess.value['currUrl'],
+    LastPageAccessTime: getFormAccess.value['bussinessDt'],
+    LocalIp:localIp,
+    RequestObject: data
   }
 }
 }
