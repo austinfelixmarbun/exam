889a05e80f899e8e40f5cca5a0c81a790963df2e
Danang Argasuma <danang.argasuma@ad-ins.com>
2019-02-13 18:17:51 +0700

    menambah searcprospect verify component

--- src/app/prospect-verif/prospect-verif.component.ts
+++ src/app/prospect-verif/prospect-verif.component.ts
@@ -1,42 +1,75 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import {SearchComponent} from '../shared/search/search.component';
-
+import { SearchComponent } from '../shared/search/search.component';
+import { NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Http, Response } from '@angular/http';
+import { data } from 'app/shared/data/smart-data-table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 
 @Component({
   selector: 'app-prospect-verif',
   templateUrl: './prospect-verif.component.html',
-  styleUrls: ['./prospect-verif.component.scss']
+  styleUrls: ['./prospect-verif.component.scss'],
+  providers: [NgbPaginationConfig, NGXToastrService] // add NgbPaginationConfig to the component providers
 })
 export class ProspectVerifComponent implements OnInit {
   @ViewChild(SearchComponent) searchComponent;
-  textbox1: string = "asd";
+  textbox1: string = 'asd';
   urlJson: string = './assets/search/searchprospectverif.json';
   configuration: any;
+  resultData: string;
+  pageNow: any;
+  totalData: any;
+  pageSize: any;
 
-  constructor() { }
+  constructor(private http: Http, private spinner: NgxSpinnerService,  private service: NGXToastrService) { }
 
   ngOnInit() {
+    this.pageNow = 1;
+    this.pageSize = 25;
   }
-  search() {
-    console.log("Call Search");
-    this.searchComponent.callSearch(1,25,null);
-    // console.log(this.searchComponent.myForm);
-    // console.log(this.searchComponent.countForm);
-    // for (var i = 0; i < this.searchComponent.countForm; i++) {
-    //   var component = this.searchComponent.myForm.nativeElement[i];
-    //   console.log(component);
-    //   if(component.nodeName ==='SELECT')
-    //   {
-    //     var ddl = component.options;
-    //     var text = ddl[ddl.selectedIndex].value;
-    //     console.log(component.name + " - " + text);
-    //   }
-    //   else{
-    //     console.log(component.name + " - " + component.value);
-    //   }
 
-    // }
-  }
+  search() {
+    this.spinner.show();
+    this.searchComponent.callSearch(this.pageNow, this.pageSize, null)
+        .subscribe(
+            (response) => {
+                console.log("Success");
+                this.resultData = response;
+                this.totalData = response.count;
+                console.log(response);
+                this.spinner.hide();
+            },
+            (error) => {
+                console.log("Error");
+                console.log(error);
+                this.spinner.hide();
+            }
+        );
+}
+
+pageChange(page:number)
+{
+    this.pageNow = page;
+    this.search();
+}
+
+// Success Type
+typeSuccess(){
+    this.service.typeSuccess();
+}
+
+typeError() {
+    this.service.typeError();
+}
+
+timeout() {
+    this.service.timeout();
+}
+
+errMsg() {
+    this.service.errorMessage('asdasd');
+}
 }
