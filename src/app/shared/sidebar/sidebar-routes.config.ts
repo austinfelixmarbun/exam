dcfe1b447c4799a90de896efd46d0cc1ab6985f6
Jovi Tanato S <jovi.ts@ad-ins.com>
2019-02-14 17:35:02 +0700

    merubah css untuk beberapa form.

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -31,7 +31,7 @@ export const ROUTES: RouteInfo[] = [
     {
         path: '', title: 'Organization Struct', icon: 'ft-bar-chart-2', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false, 
         submenu: [
-            { path: '/organization/refjobtitle', title: 'Job Title', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] }
+            { path: '/organization/refjobtitle', title: 'Organization', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] }
         ]
     },
     {
