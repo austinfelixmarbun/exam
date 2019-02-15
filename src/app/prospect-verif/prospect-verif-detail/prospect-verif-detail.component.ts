411825c1b0323b88ae37875d9fbdf669f419123a
Danang Argasuma <danang.argasuma@ad-ins.com>
2019-02-15 11:30:23 +0700

    penambahan prospect verif detail

---
+++ src/app/prospect-verif/prospect-verif-detail/prospect-verif-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prospect-verif-detail',
+  templateUrl: './prospect-verif-detail.component.html',
+  styleUrls: ['./prospect-verif-detail.component.scss']
+})
+export class ProspectVerifDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
