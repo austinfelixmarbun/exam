a3eb5ff0d791b2f1a62677c399a908fbd545850e
Jovi Tanato S <jovi.ts@ad-ins.com>
2019-02-19 13:59:31 +0700

    merge

--- src/app/organization/organization-routing.module.ts
+++ src/app/organization/organization-routing.module.ts
@@ -2,14 +2,11 @@ import { OrganizationComponent } from './organization.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RefJobTitleComponent } from './ref-job-title/ref-job-title.component';
-<<<<<<< HEAD
 import { BusinessUnitComponent } from './business-unit/business-unit.component';
 import { AddBusinessUnitComponent } from './business-unit/add/add-business-unit.component';
-=======
 import { RefJobTitleAddComponent } from './ref-job-title/ref-job-title-add/ref-job-title-add.component';
 
 
->>>>>>> 1aca9ded3280d44ad7d6803252896bbf140ff93e
 
 const routes: Routes = [
   {
