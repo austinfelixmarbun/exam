3fa03021b4964e04149f359150317ecef5f00aef
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2019-03-14 09:46:09 +0700

    refjobtitle edit

--- src/app/organization/ref-job-title/ref-job-title-add/ref-job-title-add.component.ts
+++ src/app/organization/ref-job-title/ref-job-title-add/ref-job-title-add.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'environments/environment';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { RefJobTitleObj } from 'app/shared/model/RefJobTitle.model';
 import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ref-job-title-add',
@@ -15,12 +16,17 @@ export class RefJobTitleAddComponent implements OnInit {
   type: string = "Add";
   param: string;
   resultData: any;
-  jobTitleId: any
-  apiUrl: any
+  jobTitleId: any;
+  JobTitleCode: any;
+  JobTitleName: any;
+  Descr: any;
+  apiUrl: any;
+  editUrl: any;
   foundationUrl: string = environment.foundationUrl;
 
   constructor(private route: ActivatedRoute, private httpClient: HttpClient) {
     this.apiUrl = this.foundationUrl + AdInsConstant.GetRefJobTitleById; 
+    this.editUrl = this.foundationUrl + AdInsConstant.EditRefJobTitle; 
 
     this.route.queryParams.subscribe(params => {
       if (params['param'] != null) {
@@ -43,6 +49,9 @@ export class RefJobTitleAddComponent implements OnInit {
         console.log("Success");
         this.resultData = response['returnObject'];
         console.log(this.resultData);
+        this.JobTitleCode = response['returnObject']['jobTitleCode']
+        this.JobTitleName = response['returnObject']['jobTitleName']
+        this.Descr = response['returnObject']['descr']
       },
       (error) => {
         console.log("Error");
@@ -52,4 +61,24 @@ export class RefJobTitleAddComponent implements OnInit {
     }
   }
 
+  SaveForm(ReqForm: NgForm) {
+    var formInput = this.resultData;
+    formInput.jobTitleCode = ReqForm.value.JobTitleCode;
+    formInput.jobTitleName = ReqForm.value.JobTitleName;
+    formInput.descr = ReqForm.value.Descr;
+
+    console.log(JSON.stringify(formInput))
+    console.log(formInput);
+    
+    this.httpClient.post(this.editUrl, formInput).subscribe(
+      (response) => {
+        console.log("Success");
+        console.log(response);
+      },
+      (error) => {
+        console.log("Error");
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/shared/AdInstConstant.ts
+++ src/app/shared/AdInstConstant.ts
@@ -9,6 +9,7 @@ export class AdInsConstant{
     public static GetBusinessUnitPaging = "/OrganizationDefinition/GetRefBizUnitPaging";
     public static GetRefBizUnit = "/OrganizationDefinition/GetRefBizUnit";
     public static GetRefJobTitle = "/OrganizationDefinition/GetRefJobTitlePaging";
+    public static EditRefJobTitle = "/OrganizationDefinition/EditRefJobTitle";
     public static GetRefOrgPaging = "/OrganizationDefinition/GetRefOrgPaging";
     public static GetRefJobTitleById = "/OrganizationDefinition/GetRefJobTitleByRefJobTitleId";
     public static GetBankPaging = "/RefBank/GetRefBankPaging";
