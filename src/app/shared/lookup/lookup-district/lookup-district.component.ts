df87c43f86aa2d03dc9fb6d4cba8f522586bc899
Davin Julian <davin.julian@ad-ins.com>
2019-07-12 11:56:00 +0700

    Perbaikan lookup district dan lookup ref bank mandatory

--- src/app/shared/lookup/lookup-district/lookup-district.component.ts
+++ src/app/shared/lookup/lookup-district/lookup-district.component.ts
@@ -4,22 +4,26 @@ import { NgbModal, ModalDismissReasons, NgbPaginationConfig } from '@ng-bootstra
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 import { environment } from 'environments/environment';
+import { UCSearchComponent } from '@adins/ucsearch';
+import { UcgridfooterComponent } from '@adins/ucgridfooter';
+import { InputSearchObj } from 'app/shared/model/InputSearchObj.Model';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-lookupdistrict',
   templateUrl: './lookup-district.component.html',
-  providers: [NgbPaginationConfig]
+  providers: [NgbPaginationConfig],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
 })
 export class LookupDistrictComponent implements OnInit {
+  constructor(private modalService: NgbModal) { }
 
-  urlJson: string = "./assets/lookup/lookupDistrict.json";
-  @Input() _url: string;
-  @Input() nameSelect: any = "Search ...";
-  @Input() idSelect: any;
-  @Input() jsonSelect:string;
-  @ViewChild(SearchComponent) searchComponent;
-  @ViewChild('content') contentTemplate;
-
+  @Input() parentForm: any;
+  @Input() lookupInput: any;
+  @ViewChild(UCSearchComponent) searchComponent;
+  @ViewChild("content") contentTemplate;
+  @ViewChild(UcgridfooterComponent) ucgridFooter;
+  inputObj : any;
   configuration: any;
   urlGet: string;
   countForm = 0;
@@ -33,18 +37,23 @@ export class LookupDistrictComponent implements OnInit {
   totalData: any;
   orderByKey: any = null;
   orderByValue: boolean = true;
-  addCrit : CriteriaObj[];
-  constructor(private modalService: NgbModal){
-    
-  }
+  msNewCatalogId:any;
 
   ngOnInit() {
-    this.addCrit = new Array();
-    this.apiUrl = environment.foundationUrl+AdInsConstant.GetRefProvDistrictPaging;
+    this.inputObj = new InputSearchObj();
+    this.inputObj._url = this.lookupInput.urlJson;
+    this.inputObj.enviromentUrl = this.lookupInput.urlEnviPaging;
+    this.inputObj.apiQryPaging = this.lookupInput.urlQryPaging;
+    this.inputObj.addCritInput = [];
+
+    this.apiUrl = this.lookupInput.urlEnviPaging + this.lookupInput.urlQryPaging;
     this.pageSize = 10;
     var critIsActive = new CriteriaObj();
-
-    critIsActive.propName = "isActive";
+    if (this.lookupInput.addCritInput == null || this.lookupInput.addCritInput == undefined) {
+      this.lookupInput.addCritInput = [];
+    }
+    
+    critIsActive.propName = "is_active";
     critIsActive.value = "1";
     critIsActive.restriction = AdInsConstant.RestrictionEq;
 
@@ -52,16 +61,19 @@ export class LookupDistrictComponent implements OnInit {
     critType.propName = "type";
     critType.value = "DIS";
     critType.restriction = AdInsConstant.RestrictionEq;
-    this.addCrit.push(critIsActive);
-    this.addCrit.push(critType);
+    this.inputObj.addCritInput.push(critIsActive);
+    this.inputObj.addCritInput.push(critType);
+    this.lookupInput.addCritInput.push(critIsActive);
+    this.lookupInput.addCritInput.push(critType);
+  
   }
 
   choose(id, name,item,searchComp) {
     console.log(id + " : " + name);
     console.log(item);
-    this.idSelect = id;
-    this.nameSelect = name;
-    this.jsonSelect = JSON.stringify(item);
+    this.lookupInput.idSelect = id;
+    this.lookupInput.nameSelect = name;
+    this.lookupInput.jsonSelect = JSON.stringify(item);
     this.resultData = "";
     searchComp.initiateForm();
     this.modalService.dismissAll();
@@ -75,21 +87,35 @@ export class LookupDistrictComponent implements OnInit {
     });
   }
 
-  search(searchComp) {
-    searchComp.search(this.apiUrl,this.pageNow,this.pageSize,null,this.addCrit)
-      .subscribe(
-        (response) => {
-          this.resultData = response.returnObject;
-          this.totalData = response.returnObject.count;
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
-  searchSort(searchComp,key) {
+  onSelect(searchComponent, event) {
+    this.pageNow = event.pageNow;
+    this.pageSize = event.pageSize;
+    this.searchPagination(searchComponent, this.pageNow);
+  }
+  searchPagination(searchComponent, event: number) {
+    this.pageNow = event;
+
+    var order = null;
+    if (this.orderByKey != null) {
+      order = {
+        key: this.orderByKey,
+        value: this.orderByValue
+      };
+    }
+    searchComponent.search(this.apiUrl, this.pageNow, this.pageSize, order, this.lookupInput.addCritInput );
+  }
+
+  searchSort(searchComp, key) {
     if (this.orderByKey == key) {
       this.orderByValue = !this.orderByValue;
     } else {
@@ -99,78 +125,17 @@ export class LookupDistrictComponent implements OnInit {
     var order = {
       key: this.orderByKey,
       value: this.orderByValue
-    }
-    searchComp.search(this.apiUrl, this.pageNow, this.pageSize, order,this.addCrit)
-      .subscribe(
-        (response) => {
-          console.log("Success");
-          this.resultData = response.returnObject;
-          this.totalData = response.returnObject.count;
-          console.log(this.resultData);
-        },
-        (error) => {
-          console.log("Error");
-          console.log(error);
-        }
-      );
+    };
+    searchComp.search(this.apiUrl, this.pageNow, this.pageSize, order, this.lookupInput.addCritInput );
   }
 
-  searchPagination(searchComp,event: number) {
-    this.pageNow = event;
-    var order = null;
-    if (this.orderByKey != null) {
-      order = {
-        key: this.orderByKey,
-        value: this.orderByValue
-      }
-    }
-    searchComp.search(this.apiUrl, this.pageNow, this.pageSize, order,this.addCrit)
-      .subscribe(
-        (response) => {
-          console.log("Success");
-          this.resultData = response.returnObject;
-          this.totalData = response.returnObject.count;
-          console.log(this.resultData);
-        },
-        (error) => {
-          console.log("Error");
-          console.log(error);
-        }
-      );
-  }
-
-  onChange(searchComp,eventValue: any) {
-
-    var order = null;
-    if (this.orderByKey != null) {
-      order = {
-        key: this.orderByKey,
-        value: this.orderByValue
-      }
-    }
-    searchComp.search(this.apiUrl, this.pageNow, this.pageSize, order,this.addCrit)
-      .subscribe(
-        (response) => {
-          console.log("Success");
-          this.resultData = response.returnObject;
-          this.totalData = response.returnObject.count;
-          console.log(this.resultData);
-        },
-        (error) => {
-          console.log("Error");
-          console.log(error);
-        }
-      );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+  getResult(ucgridFooter,event) {
+    console.log(this.lookupInput.urlQryPaging);
+    this.resultData = event.response.returnObject;
+    this.totalData = event.response.returnObject.count;
+    ucgridFooter.pageNow = event.pageNow;
+    ucgridFooter.totalData = this.totalData;
+    ucgridFooter.resultData = this.resultData;
   }
 
 }
