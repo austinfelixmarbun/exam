3d0bed1b0eec4a88f6475df3bd51d1206422425d
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2019-07-31 10:29:10 +0700

    merge

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -12,7 +12,6 @@ import { Dummy3Component } from "./dummy3/dummy3.component";
 import { DummyPagingComponent } from "./dummy/dummy-paging/dummy-paging.component";
 import { Template1Component } from './template/template1/template1.component';
 import { TestPagingComponent } from './test-paging/test-paging.component';
-import { TestPagingV2Component } from './test-paging-v2/test-paging-v2.component';
 
 @NgModule({
     imports: [
@@ -30,8 +29,7 @@ import { TestPagingV2Component } from './test-paging-v2/test-paging-v2.component
         Dummy3Component,
         DummyPagingComponent,
         Template1Component,
-        TestPagingComponent,
-        TestPagingV2Component
+        TestPagingComponent
     ],
 
 })
