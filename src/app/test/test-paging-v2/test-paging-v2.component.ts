18828c6d3d0b32aa8b27fc4aa5bf1d7275ebeba1
Davin Julian <davin.julian@ad-ins.com>
2019-08-30 11:03:25 +0700

    Perbaikan lookup required or not

--- src/app/test/test-paging-v2/test-paging-v2.component.ts
+++ src/app/test/test-paging-v2/test-paging-v2.component.ts
@@ -38,6 +38,7 @@ export class TestPagingV2Component implements OnInit {
     this.inputLookupObj.urlEnviPaging = environment.settingUrl;
     this.inputLookupObj.pagingJson = "./assets/form-setting/dummyPaging2.json";
     this.inputLookupObj.genericJson = "./assets/form-setting/dummyGeneric.json";
+    this.inputLookupObj.isRequired = true;
     //** app-lookupgeneric **//
 
 
