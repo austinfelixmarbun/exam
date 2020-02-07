35fbcd8f2c4772c077586b4d508cdcbc26cd4518
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-02-07 13:54:52 +0700

    ucAddressGroup

--- src/app/test-new/uc-address-group/uc-address-group.component.ts
+++ src/app/test-new/uc-address-group/uc-address-group.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-uc-address-group',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UcAddressGroupComponent implements OnInit {
 
-  constructor() { }
+  @Input() UCAddrForm: FormGroup;
+  @Input() enjiForm: NgForm;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log("UcAddressForm");
+    this.UCAddrForm.addControl("UCAddress" , this.fb.group({
+      Addr: ['', Validators.required],
+      AreaCode4: ['', [Validators.required, Validators.pattern("^[0-9]+$"), Validators.maxLength(3)]],
+      AreaCode3: ['', Validators.required],
+      AreaCode2: ['', Validators.required],
+      AreaCode1: ['', Validators.required],
+      City: ['', Validators.required],
+      PhnArea1: ['', [Validators.required, Validators.pattern("^[0-9]+$")]],
+      Phn1: ['', [Validators.required, Validators.pattern("^[0-9]+$")]],
+      PhnExt1: ['', Validators.pattern("^[0-9]+$")],
+      PhnArea2: ['', Validators.pattern("^[0-9]+$")],
+      Phn2: ['', Validators.pattern("^[0-9]+$")],
+      PhnExt2: ['', Validators.pattern("^[0-9]+$")],
+      PhnArea3: ['', Validators.pattern("^[0-9]+$")],
+      Phn3: ['', Validators.pattern("^[0-9]+$")],
+      PhnExt3: ['', Validators.pattern("^[0-9]+$")],
+      FaxArea: ['', Validators.pattern("^[0-9]+$")],
+      Fax: ['', Validators.pattern("^[0-9]+$")]
+    }));
   }
 
 }
