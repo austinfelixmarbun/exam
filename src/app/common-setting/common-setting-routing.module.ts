60b4c37bd26280d88b4aa2365c9b6afcb2fabeac
steven.fh <stevenfebrian1@gmail.com>
2020-03-03 13:56:12 +0700

    Benerin Profession

--- src/app/common-setting/common-setting-routing.module.ts
+++ src/app/common-setting/common-setting-routing.module.ts
@@ -216,21 +216,21 @@ const routes: Routes = [
         },
       },
       {
-        path: 'refStatus/paging',
+        path: 'RefStatus/paging',
         component: RefStatusPagingComponent,
         data: {
           title: 'Ref Status Paging'
         }
       },
       {
-        path: 'profession/paging',
+        path: 'Profession/paging',
         component: ProfessionComponent,
         data: {
           title: 'Profession Paging'
         },
       },
       {
-        path: 'profession/detail',
+        path: 'Profession/detail',
         component: ProfessionAddEditComponent,
         data: {
           title: 'Profession Add Edit'
