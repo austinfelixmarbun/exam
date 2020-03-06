62bb6eb07c513fd96abc487a7983782f5e1fb83c
Dwi Adhi Widigda Kartomihardjo <dwi.awk@ad-ins.com>
2020-03-06 17:47:24 +0700

    Ref Office Detail

---
+++ src/app/shared/model/UcAddressObj.Model.ts
@@ -0,0 +1,19 @@
+export class UcAddressObj{
+        Addr: any;
+        AreaCode4: any;
+        AreaCode3: any;
+        AreaCode2: any;
+        AreaCode1: any;
+        City: any;
+        PhnArea1: any;
+        Phn1: any;
+        PhnExt1: any;
+        PhnArea2: any;
+        Phn2: any;
+        PhnExt2: any;
+        PhnArea3: any;
+        Phn3: any;
+        PhnExt3: any;
+        FaxArea: any;
+        Fax: any;
+}
