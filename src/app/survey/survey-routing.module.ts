43d6ec2393f1931008100e21c2fd7f9572823ed6
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-03-13 17:27:54 +0700

    survey

--- src/app/survey/survey-routing.module.ts
+++ src/app/survey/survey-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SurveyOrderComponent } from './survey-order/survey-order.component';
+import { SurveyOrderTaskComponent } from './survey-order-task/survey-order-task.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,13 @@ const routes: Routes = [
           title: 'Survey Order'
         },
       },
+      {
+        path: 'Task',
+        component: SurveyOrderTaskComponent,
+        data: {
+          title: 'Survey Order Task'
+        },
+      }
     ]
   }
 ];
