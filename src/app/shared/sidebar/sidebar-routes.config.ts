1ec505eaeeb8680319372d6bc54c3c448f50aa99
Ardi Gunawan <ardi.gunawan@ad-ins.com>
2020-03-18 09:30:47 +0700

    Add Verification Menu, Module, Routing, Service.

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -75,9 +75,10 @@ export const ROUTES: RouteInfo[] = [
   {
     path: '', title: 'Verification', icon: 'ft-layers', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
     submenu: [
-      { path: '/Product/HOpaging', title: 'Question List', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-      { path: '/Product/HOpaging', title: 'Question Scheme', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] }
-      
+      { path: '/Verification/QuestionAnswerPaging', title: 'Verification Question Answer', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+      { path: '/Verification/QuestionSchemePaging', title: 'Verification Question Scheme', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+      { path: '/Verification/QuestionGroupPaging', title: 'Verification Question Group', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+      { path: '/Verification/QuestionGroupMemberPaging', title: 'Verification Question Group Member', icon: '', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] }
     ]
   },
   {    path: '', title: 'Vendor', icon: 'ft-aperture', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
---
+++ src/app/verification/verification-question-answer/verification-question-answer-paging/verification-question-answer-paging.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
+import { environment } from 'environments/environment';
+import { UcPagingObj } from 'app/shared/model/UcPagingObj.Model';
+
+@Component({
+  selector: 'app-verification-question-answer-paging',
+  templateUrl: './verification-question-answer-paging.component.html',
+  styleUrls: ['./verification-question-answer-paging.component.scss']
+})
+export class VerificationQuestionAnswerPagingComponent implements OnInit {
+
+  inputPagingObj: any;
+
+  ngOnInit() {
+    this.inputPagingObj = new UcPagingObj();
+    this.inputPagingObj._url = "./assets/ucpaging/verification/searchVerificationQuestionAnswer.json";
+    this.inputPagingObj.enviromentUrl = environment.FoundationR3Url;
+    this.inputPagingObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
+    this.inputPagingObj.pagingJson = "./assets/ucpaging/verification/searchVerificationQuestionAnswer.json";
+    this.inputPagingObj.deleteUrl = AdInsConstant.DeleteRefOffice;
+  }
+}
