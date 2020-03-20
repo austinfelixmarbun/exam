bac68c7da8bd5c9cf25b7b18ab66f1b2a9854485
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-03-20 10:30:15 +0700

    missing console log

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-  if(window){
-    window.console.log=function(){};
-  }
-}
+// if (environment.production) {
+//   enableProdMode();
+//   if(window){
+//     window.console.log=function(){};
+//   }
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
