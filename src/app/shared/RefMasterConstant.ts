5af7ec8d588320b2550f5c82de224b059f6e1929
Hartono Rachmat <hartono.rachmat@ad-ins.com>
2020-03-26 16:09:15 +0700

    merging randy git

--- src/app/shared/RefMasterConstant.ts
+++ src/app/shared/RefMasterConstant.ts
@@ -3,9 +3,5 @@ export class RefMasterConstant {
     public static CustModel = "CUST_MODEL";
     public static Gender = "GENDER";
     public static AddrType = "ADDR_TYPE";
-<<<<<<< HEAD
-    public static LegalAddr =  "LEGAL"
-=======
-    public static LegalAddr = "LEGAL"
->>>>>>> 8eeab6c297edbe51aa908b27ed861f3ca0f0ca8e
+    public static LegalAddr = "LEGAL";
 }
