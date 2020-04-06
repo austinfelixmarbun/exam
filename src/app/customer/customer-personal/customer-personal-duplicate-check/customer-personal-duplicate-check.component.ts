aaf7d80898ed393769b79a4948695d14f7f77f33
Ardi Gunawan <ardi.gunawan@ad-ins.com>
2020-04-06 14:59:55 +0700

    Add BirthDt and MotherMaidenName into DuppCustP

--- src/app/customer/customer-personal/customer-personal-duplicate-check/customer-personal-duplicate-check.component.ts
+++ src/app/customer/customer-personal/customer-personal-duplicate-check/customer-personal-duplicate-check.component.ts
@@ -104,6 +104,8 @@ export class CustomerPersonalDuplicateCheckComponent implements OnInit {
     this.DuplicateCustObj.CustName = this.CustName;
     this.DuplicateCustObj.MrCustTypeCode = RefMasterConstant.Personal;
     this.DuplicateCustObj.IdNo = this.IdNo;
+    this.DuplicateCustObj.MotherMaidenName = this.MotherMaidenName;
+    this.DuplicateCustObj.BirthDt = this.BirthDt;
     console.log(this.DuplicateCustObj);
     this.http.post(AdInsConstant.GetCustomerAndNegativeCustDuplicateCheck, this.DuplicateCustObj).subscribe(
       (response) => {
