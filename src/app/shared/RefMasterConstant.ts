5bdf64fb9cced35de63e7c264b383dc2fa21d493
Ardi Gunawan <ardi.gunawan@ad-ins.com>
2020-04-06 08:54:17 +0700

    Add RefMasterConstant Npwp

--- src/app/shared/RefMasterConstant.ts
+++ src/app/shared/RefMasterConstant.ts
@@ -5,6 +5,7 @@ export class RefMasterConstant {
     public static AddrType = "ADDR_TYPE";
     public static LegalAddr = "LEGAL";
     public static EKtp = "EKTP";
+    public static Npwp = "NPWP";
     public static Company = "COMPANY";
     public static Personal = "PERSONAL";
 }
