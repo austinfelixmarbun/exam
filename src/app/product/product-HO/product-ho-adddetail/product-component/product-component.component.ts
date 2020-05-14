e9413ca70ba4c1dd5662bb1a0c745e621283bb95
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-05-14 10:01:48 +0700

    tambah pointer

--- src/app/product/product-HO/product-ho-adddetail/product-component/product-component.component.ts
+++ src/app/product/product-HO/product-ho-adddetail/product-component/product-component.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormArray, FormGroup, Validators } from '@angular/forms';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
-import { InputLookupObj } from 'app/shared/model/InputLookupObj.Model';
-import { environment } from 'environments/environment';
-import { RefProductDetailObj } from 'app/shared/model/RefProductDetailObj.Model';
 import { WizardComponent } from 'angular-archwizard';
-import { ListRefProductDetailObj } from 'app/shared/model/ListRefProductDetailObj.Model';
 import { saveAs } from 'file-saver';
 
 @Component({
@@ -21,7 +17,6 @@ export class ProductComponentHOComponent implements OnInit {
   @Input() objInput: any;
 
   constructor(
-    private route: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
     private fb: FormBuilder,
