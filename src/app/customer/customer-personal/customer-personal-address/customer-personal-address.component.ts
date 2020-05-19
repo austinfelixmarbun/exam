000767cb48eac94f9030ead27d382a55e2a88bcf
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-05-19 13:21:38 +0700

    Customer

--- src/app/customer/customer-personal/customer-personal-address/customer-personal-address.component.ts
+++ src/app/customer/customer-personal/customer-personal-address/customer-personal-address.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core'; 
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service'; 
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
+import { CustAddrObj } from 'app/shared/model/CustAddrObj.Model';
  
  
 @Component({
@@ -13,22 +17,55 @@ export class CustomerPersonalAddressComponent implements OnInit {
   @Output() outputTab: EventEmitter<object> = new EventEmitter();
   mode: string;
   AddrId: number;
-  
-  constructor( ) {  
+  IdCust : number;
+  legalAddr : any;
+  residenceAddr : any;
+  custAddrObj : CustAddrObj;
+  constructor( private http: HttpClient, private route: ActivatedRoute, private toastr: NGXToastrService) {  
+    this.route.queryParams.subscribe(params => {
+      if (params["IdCust"] != null) {
+         this.IdCust = params["IdCust"];
+       }
+     });
   }
 
   ngOnInit() { 
-    this.mode = "check";
+    this.mode = "check";  
   }
   terimaValue(ev){
     console.log(ev);
     this.mode = ev.mode; 
     this.AddrId =  ev.AddrId;
-    console.log("testing");
+    
   
   }
-  next() {
-    this.outputTab.emit({ stepMode: "next"});
+  next() { 
+    this.custAddrObj = new CustAddrObj();
+    this.custAddrObj.MrCustAddrTypeCode= "LEGAL";
+    this.custAddrObj.CustId = this.IdCust;
+    this.http.post(AdInsConstant.GetCustAddrByMrCustAddrType, this.custAddrObj).subscribe(
+      (response) => { 
+        this.legalAddr = response; 
+        this.custAddrObj.MrCustAddrTypeCode = "RESIDENCE";
+        this.http.post(AdInsConstant.GetCustAddrByMrCustAddrType, this.custAddrObj).subscribe(
+          (response) => { 
+            this.residenceAddr = response; 
+            if(this.legalAddr.Addr==null || this.residenceAddr.Addr == null){
+              if(this.legalAddr.Addr!=null && this.residenceAddr.Addr == null){
+                this.toastr.errorMessage("Please complete Residence Address First");
+              }else if( this.legalAddr.Addr==null && this.residenceAddr.Addr != null){
+                this.toastr.errorMessage("Please complete Legal Address First");
+              }else{
+                this.toastr.errorMessage("Please complete Legal and Residenec Address First");
+              }
+
+            } 
+            if(this.legalAddr.Addr!=null && this.residenceAddr.Addr != null)
+            {
+              this.outputTab.emit({ stepMode: "next"});
+            } 
+          }); 
+      }); 
   }
   back(){
     this.outputTab.emit({ stepMode: "previous"});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,8 @@ export const environment = {
   r2AppServerUrl: 'http://R2AppServer/POC/api',
   //losUrl : 'http://r3web-server.ad-ins.com/LOSR3/pages/login',
   losUrl : 'http://localhost:4300/pages/login',
-  //FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
-  FoundationR3Url: 'http://localhost:5000',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
+  //FoundationR3Url: 'http://localhost:5000',
   Module:"FOU",
   ApprovalURL : 'http://r3app-server/APPROVAL'
 };
