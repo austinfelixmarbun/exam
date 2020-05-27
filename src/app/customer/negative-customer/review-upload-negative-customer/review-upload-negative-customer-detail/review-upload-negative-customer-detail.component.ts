3a0df87b2ddd65e8fa109e46cb882b5fbeb39c4d
Vincent Andreas <vincent.andreas@ad-ins.com>
2020-05-27 18:38:18 +0700

    membuat review upload negative customer

--- src/app/customer/negative-customer/review-upload-negative-customer/review-upload-negative-customer-detail/review-upload-negative-customer-detail.component.ts
+++ src/app/customer/negative-customer/review-upload-negative-customer/review-upload-negative-customer-detail/review-upload-negative-customer-detail.component.ts
@@ -35,6 +35,7 @@ export class ReviewUploadNegativeCustomerDetailComponent implements OnInit {
     this.viewUpload = "./assets/ucviewgeneric/viewReviewUploadNegativeCust.json";
   }
   ngOnInit() {
+    console.log('test')
     this.claimTask();
     this.UploadReviewUrl = AdInsConstant.UploadReview;
     this.CancelUpload = AdInsConstant.CancelUpload;
@@ -69,7 +70,7 @@ export class ReviewUploadNegativeCustomerDetailComponent implements OnInit {
     );
   }
   claimTask() {
-    var currentUserContext = JSON.parse(localStorage.getItem("UserContext"));
+    var currentUserContext = JSON.parse(localStorage.getItem("UserAccess"));
     var wfClaimObj = { pWFTaskListID: this.taskListId, pUserID: currentUserContext["UserName"] };
     console.log(wfClaimObj);
     this.http.post(AdInsConstant.ClaimTask, wfClaimObj).subscribe(
