a084183f9213ed361d09cfc5850a931038d4847b
Dwi Adhi Widigda Kartomihardjo <dwi.awk@ad-ins.com>
2020-06-03 10:37:37 +0700

    merge commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
   localHostUrl: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   r2AppServerUrl: 'http://R2AppServer/POC/api',
   LosURL : 'http://r3web-server.ad-ins.com/LOSR3/',
-  //FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
-  FoundationR3Url: 'http://localhost:5000',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
+  //FoundationR3Url: 'http://localhost:5000',
   Module:"FOU",
   ApprovalURL : 'http://r3app-server/APPROVAL'
 };
