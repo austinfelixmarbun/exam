aa9fc0cf196f97cb71186552a090522ea566b4f0
Steven <stevenfebrian1@gmail.com>
2020-06-04 16:15:24 +0700

    Update url

--- src/app/office/office-add/office-add.component.ts
+++ src/app/office/office-add/office-add.component.ts
@@ -154,7 +154,7 @@ export class OfficeAddComponent implements OnInit {
     this.InputLookupObj = new InputLookupObj();
     this.InputLookupObj.urlJson = "./assets/lookup/lookupOfficeParent.json";
     this.InputLookupObj.urlQryPaging = "/Generic/GetPagingObjectBySQL";
-    this.InputLookupObj.urlEnviPaging = "http://r3app-server.ad-ins.com/Foundation";
+    this.InputLookupObj.urlEnviPaging = "http://r3app-server.ad-ins.com/Foundation_R3";
     this.InputLookupObj.pagingJson = "./assets/lookup/lookupOfficeParent.json";
     this.InputLookupObj.genericJson = "./assets/lookup/lookupOfficeParent.json";
 
