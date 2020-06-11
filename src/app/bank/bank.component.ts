52fe97015e1b5ce6c79e6685914139fe7b5b7f49
Xania Yoan Apsari <xania.ya@ad-ins.com>
2020-06-11 17:35:35 +0700

    PerubahanReject

--- src/app/bank/bank.component.ts
+++ src/app/bank/bank.component.ts
@@ -17,6 +17,7 @@ export class BankComponent implements OnInit {
   inputPagingObj: any;
 
   ngOnInit() {
+    //PERUBUAHAN HABIS REJECT
     this.inputPagingObj = new UcPagingObj();
     this.inputPagingObj._url = "./assets/ucpaging/searchBank.json";
     this.inputPagingObj.enviromentUrl = environment.FoundationR3Url;
