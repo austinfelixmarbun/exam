527154403d08bbdd8c425d12a861d1deca735382
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-06-11 19:33:08 +0700

    test push master 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
 
     private _hubConnection: HubConnection;
     msgs: Message[] = [];
-    //TEST PUSH MASTER 3
+    //TEST PUSH MASTER 4
     constructor() { }
  
     ngOnInit(): void {
