c4d73458a5a7093af44f33bc202654e6e0ab6746
Andre Sucipto <andre.sucipto@ad-ins.com>
2020-06-12 18:48:34 +0700

    commit

--- src/app/product/product-HO/product-return-ho-paging/product-return-ho-paging.component.ts
+++ src/app/product/product-HO/product-return-ho-paging/product-return-ho-paging.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'environments/environment';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 import { Router, ActivatedRoute } from '@angular/router';
+import { UcPagingObj } from '../../../shared/model/UcPagingObj.Model';
 
 @Component({
   selector: 'app-product-return-ho-paging',
@@ -18,12 +19,11 @@ export class ProductReturnHoPagingComponent implements OnInit {
 
   inputPagingObj;
   ngOnInit() {
-    this.inputPagingObj=new UcpagingModule();
-    this.inputPagingObj._url="./assets/ucpaging/product/searchProductHO.json";
+    this.inputPagingObj = new UcPagingObj();
+    this.inputPagingObj._url="./assets/ucpaging/product/searchProductHOReturn.json";
     this.inputPagingObj.enviromentUrl = environment.FoundationR3Url;
     this.inputPagingObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
-    // this.inputPagingObj.deleteUrl = "/RefBank/DeleteRefBank";
-    this.inputPagingObj.pagingJson = "./assets/ucpaging/product/searchProductHO.json";
+    this.inputPagingObj.pagingJson = "./assets/ucpaging/product/searchProductHOReturn.json";
 
     var criteriaList = new Array<CriteriaObj>();
     var criteriaObj = new CriteriaObj();
