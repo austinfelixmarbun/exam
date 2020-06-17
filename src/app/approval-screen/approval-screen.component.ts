7a58040f708b4cd9e8af844324f6349b73bf43f3
ifvan <ifvan@ifvan.com>
2020-06-17 14:26:25 +0700

    ubah css dan tambah info apv value di apv smry

--- src/app/approval-screen/approval-screen.component.ts
+++ src/app/approval-screen/approval-screen.component.ts
@@ -26,8 +26,8 @@ export class ApprovalScreenComponent implements OnInit {
 
   ngOnInit() {
     var obj = {
-      taskId : 75659,
-      instanceId : 70487,
+      taskId : 75786,
+      instanceId : 70605,
       approvalBaseUrl : environment.ApprovalURL
     }
 
