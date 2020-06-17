3c3137f7a7f76644224cd9b5bb5ec88f3cfb43b4
Xania Yoan Apsari <xania.ya@ad-ins.com>
2020-06-17 16:47:59 +0700

    Update setting lookup HO n Branch

--- src/app/shared/AdInstConstant.ts
+++ src/app/shared/AdInstConstant.ts
@@ -552,7 +552,7 @@ export class AdInsConstant {
   public static DeleteVendorHO = "/Vendor/DeleteVendorHO";
   public static AddVendorHolding = environment.FoundationR3Url + "/Vendor/AddVendorHolding";
   public static EditVendorHolding = environment.FoundationR3Url + "/Vendor/EditVendorHolding";
-  public static GetVendorHOAndVendorAddr = environment.FoundationR3Url + "/Vendor/GetVendorAndVendorTaxAddrByVendorId";
+  public static GetVendorAndVendorAddr = environment.FoundationR3Url + "/Vendor/GetVendorAndVendorTaxAddrByVendorId";
   public static GetVendorByVendorId = environment.FoundationR3Url + "/Vendor/GetVendorByVendorId";
   public static AddVendorAddr = environment.FoundationR3Url + "/VendorAddr/AddVendorAddr";
   public static EditVendorAddr = environment.FoundationR3Url + "/VendorAddr/EditVendorAddr";
