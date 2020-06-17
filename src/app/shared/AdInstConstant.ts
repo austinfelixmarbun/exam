ac399904b588e67c396ccb9ae8b0e6054d7fbf81
ifvan <ifvan@ifvan.com>
2020-06-18 06:52:09 +0700

    SITCFRTHREE - Tambah mekanisme Ret Notes

--- src/app/shared/AdInstConstant.ts
+++ src/app/shared/AdInstConstant.ts
@@ -472,6 +472,7 @@ export class AdInsConstant {
   public static GetProductDetailComponentInfo = environment.FoundationR3Url + "/Product/GetProductDetailComponentInfo";
   public static AddOrEditProductDetail = environment.FoundationR3Url + "/Product/AddOrEditProductDetail";
   public static DownloadProductRule = environment.FoundationR3Url + "/Product/DownloadProductRule";
+  public static UpdateProductPostApv = environment.FoundationR3Url + "/Product/UpdateProductPostApv";
 
   //PRODUCT OFFERING
   public static GetProductOfferingMainInfo = environment.FoundationR3Url + "/ProductOffering/GetProductOfferingMainInfo";
@@ -488,7 +489,7 @@ export class AdInsConstant {
   public static RequestOfferingDeactivation = environment.FoundationR3Url + "/ProductOffering/RequestProdOfferingDeactivationProdOffering"
   public static GetListProdOfferingBranchOfficeMbrByProdHIdAndApp = environment.FoundationR3Url + "/ProductOffering/GetListProdOfferingBranchOfficeMbrByProdHIdAndApp"
   public static CopyProductOffering = environment.FoundationR3Url + "/ProductOffering/CopyProductOffering";
-
+  public static UpdateProdOfferingPostApv = environment.FoundationR3Url + "/ProductOffering/UpdateProdOfferingPostApv";
 
   // PRODUCT COMPONENT
   public static GetProductHOComponent = environment.FoundationR3Url + "/ProductComponent/GetProductHOComponent";
