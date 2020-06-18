3f444a25fc42155b97a1b9bcedf9df5da960f86d
Jacksen <jacksen@ad-ins.com>
2020-06-18 14:17:32 +0700

    revisi critObj

--- src/app/verification/verification-question-scheme-member/verification-question-scheme-member-add/verification-question-scheme-member-add.component.ts
+++ src/app/verification/verification-question-scheme-member/verification-question-scheme-member-add/verification-question-scheme-member-add.component.ts
@@ -77,16 +77,12 @@ export class VerificationQuestionSchemeMemberAddComponent implements OnInit {
     this.inputObj.enviromentUrl = environment.FoundationR3Url;
     this.inputObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
 
-    var critObj = new CriteriaObj();
-    critObj.restriction = AdInsConstant.RestrictionLike;
-    critObj.propName = 'VERF_QUESTION_GRP_H_ID';
-    critObj.value = this.VerfSchemeHId.toString();
-    this.arrCrit.push(critObj);
-    this.inputObj.critObj = this.arrCrit;
+   
+
     this.pageNow = 1;
     this.pageSize = 10;
     this.apiUrl = environment.FoundationR3Url + AdInsConstant.GetPagingObjectBySQL;
-    this.inputObj.addCritInput = new Array();
+    
 
     var verfSchemeObj = { VerfSchemeHId: this.VerfSchemeHId }
     this.http.post(AdInsConstant.GetVerfSchemeHById, verfSchemeObj).subscribe(
@@ -263,7 +259,7 @@ export class VerificationQuestionSchemeMemberAddComponent implements OnInit {
         if(arrMemberList.length != 0){
           const addCritListVerfQuestionGrpHId = new CriteriaObj();
           addCritListVerfQuestionGrpHId.DataType = "numeric";
-          addCritListVerfQuestionGrpHId.propName = "VERF_QUESTION_GRP_H_ID";
+          addCritListVerfQuestionGrpHId.propName = "verf.VERF_QUESTION_GRP_H_ID";
           addCritListVerfQuestionGrpHId.restriction = AdInsConstant.RestrictionNotIn;
           addCritListVerfQuestionGrpHId.listValue = arrMemberList;
           this.arrCrit.push(addCritListVerfQuestionGrpHId);
