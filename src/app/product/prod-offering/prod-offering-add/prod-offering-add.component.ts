8677254a519923811b41ed68cc14343693f5ca43
Jason Andreas Widjaja <jason.aw@ad-ins.com>
2020-07-06 15:39:56 +0700

    prod offering

--- src/app/product/prod-offering/prod-offering-add/prod-offering-add.component.ts
+++ src/app/product/prod-offering/prod-offering-add/prod-offering-add.component.ts
@@ -209,12 +209,12 @@ export class ProdOfferingAddComponent implements OnInit {
     let endDate = new Date(this.ProdOfferingForm.get("EndDt").value);
 
     if (startDate > endDate) {
-      this.toastr.errorMessage("Start Date Must be Less than End Date");
+      this.toastr.warningMessage("Start Date Must be Less than End Date");
       return false;
     }
 
     if (endDate <= businessDate) {
-      this.toastr.errorMessage("End Date Must be Greater than Business Date");
+      this.toastr.warningMessage("End Date Must be Greater than Business Date");
       return false;
     }
     return true;
