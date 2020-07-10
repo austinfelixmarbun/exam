71238d9978cb2fbcf9853a1ce26e92ede44ea941
Ardi Gunawan <ardi.gunawan@ad-ins.com>
2020-07-10 12:13:08 +0700

    Add typescript-string-operations

--- src/app/shared/constant/CommonConstant.ts
+++ src/app/shared/constant/CommonConstant.ts
@@ -1,6 +1,9 @@
 
 export class CommonConstant {
 
+    // APPLICATION DATA
+    public static ReturnObj = 'ReturnObject';
+
     // VENDOR CATEGORY
     public static SUPPLIER_BRANCH = "SUPPLIER_BRANCH";
     public static ASSET_INSCO_BRANCH = "ASSET_INSCO_BRANCH";
@@ -13,5 +16,14 @@ export class CommonConstant {
     public static SUPPLIER_HO = "SUPPLIER_HO";
     public static SURVEYOR_HO = "SURVEYOR_HO";
     public static SUPPLIER_HOLDING = "SUPPLIER_HOLDING";
+
+    public static FROM = 'From';
+    public static TO = 'To';
+    public static BETWEEN = 'In Between';
+
+    public static GTE = "Greater Than Equal";
+    public static GT = "Greater Than";
+    public static LTE = "Less Than Equal";
+    public static LT = "Less Than";
 
 }
