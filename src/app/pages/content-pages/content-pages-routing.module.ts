5f8b9f139f74b5fea6b624dba3826b1b88185423
Andre Sucipto <andre.sucipto@ad-ins.com>
2020-07-29 14:38:10 +0700

    penambahan reset password

--- src/app/pages/content-pages/content-pages-routing.module.ts
+++ src/app/pages/content-pages/content-pages-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from "app/pages/content-pages/login/login-page.component";
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { PagesComponent } from './pages/pages.component';
-
+import { RequestNewPasswordComponent } from './request-new-password/request-new-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 const routes: Routes = [
   {
@@ -28,6 +29,20 @@ const routes: Routes = [
         component: ChangePasswordComponent,
         data: {
           title: 'Change Password Page'
+        }
+      },
+      {
+        path: 'RequestPassword',
+        component: RequestNewPasswordComponent,
+        data: {
+          title: 'Request New Password Page'
+        }
+      },
+      {
+        path: 'ResetPassword/:code',
+        component: ResetPasswordComponent,
+        data: {
+          title: 'Reset Password Page'
         }
       }
     ]
