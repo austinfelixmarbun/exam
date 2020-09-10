54db20260d12cee09c03cbc56770af8679170d24
Edward Bala Yansen <edward.by@ad-ins.com>
2020-09-10 12:06:50 +0700

    error exception copy holiday

--- src/app/common-setting/holiday-scheme/holiday-detail/holiday-detail.component.ts
+++ src/app/common-setting/holiday-scheme/holiday-detail/holiday-detail.component.ts
@@ -84,10 +84,17 @@ export class HolidayDetailComponent implements OnInit {
       this.copyHoliday.HolidaySchmHId = this.HolidaySchmHId;
       this.copyHoliday.HolidaySchmHIdCopy = this.HolidaySchmHIdCopy;
 
-      this.http.post(URLConstant.CopyHolidaySchmH, this.copyHoliday).subscribe((response) => {
-        this.router.navigate(['/CommonSetting/Holiday/Detail/'], { queryParams: { HolidaySchmHId: this.HolidaySchmHId } });
-        this.toastr.successMessage(response['message']);
-      });
+      this.http.post(URLConstant.CopyHolidaySchmH, this.copyHoliday).subscribe(
+        (response) => {
+          console.log("success");
+          this.router.navigate(['/CommonSetting/Holiday/Detail/'], { queryParams: { HolidaySchmHId: this.HolidaySchmHId } });
+          this.toastr.successMessage(response['message']);
+        },
+        (error) => {
+          console.log("error");
+          this.toastr.warningMessage(error['message']);
+        }
+      );
     }
   }
 }
