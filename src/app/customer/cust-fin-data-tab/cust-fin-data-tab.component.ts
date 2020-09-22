4251449cdd7ef610ff152bcb0986ab25b115c4d1
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-09-22 18:49:12 +0700

    Backlog2928

--- src/app/customer/cust-fin-data-tab/cust-fin-data-tab.component.ts
+++ src/app/customer/cust-fin-data-tab/cust-fin-data-tab.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RefMasterObj } from 'app/shared/model/RefMasterObj.Model';
 import { CustPersonalFinDataObj } from 'app/shared/model/CustPersonalFinDataObj.Model';
 import { CustCompanyFinDataObj } from 'app/shared/model/CustCompanyFinDataObj.Model';
 import { CustPersonalObj } from 'app/shared/model/CustPersonalObj.Model';
-import { map, mergeMap } from 'rxjs/operators';
+import { first, map, mergeMap } from 'rxjs/operators';
 import { forkJoin } from 'rxjs';
 import { CustCompanyObj } from 'app/shared/model/CustCompanyObj.Model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { CommonConstant } from 'app/shared/constant/CommonConstant';
 import { URLConstant } from 'app/shared/constant/URLConstant';
+import { InputLookupObj } from 'app/shared/model/InputLookupObj.Model';
+import { environment } from 'environments/environment';
+import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
 
 @Component({
   selector: 'app-cust-fin-data-tab',
@@ -24,13 +28,18 @@ export class CustFinDataTabComponent implements OnInit {
   @Input() MrCustTypeCode: string;
   @Input() CustId: number;
   @Output() outputTab: EventEmitter<object> = new EventEmitter();
-  
+
   sourceOfIncomeList: any;
   isCalculated: boolean;
   spouseMonthlyIncomeAmt: number;
   mrMaritalStatCode: string;
   maritalConstant: string = CommonConstant.MR_MARITAL_STAT_CODE_MARRIED;
-  Page : string;
+  Page: string;
+  RefAttrList: any;
+  ListAttrContent: any;
+  isRefAttrFormReady: boolean = false;
+  ListInputLookUpObj = new Array();
+  attrGroup:string;
   CustPersonalFinDataForm = this.fb.group({
     CustPersonalFinDataId: [0, [Validators.required]],
     CustPersonalId: [0, [Validators.required]],
@@ -78,27 +87,32 @@ export class CustFinDataTabComponent implements OnInit {
     RowVersion: ['']
   });
 
+  RefAttrForm: FormGroup;
+  isAdd: boolean = true;
   constructor(
     private httpClient: HttpClient,
     private toastr: NGXToastrService,
-    private fb: FormBuilder, 
+    private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute
   ) {
-    if(this.MrCustTypeCode == CommonConstant.CustTypePersonal){
+    if (this.MrCustTypeCode == CommonConstant.CustTypePersonal) {
       this.isCalculated = false;
     }
-    else if(this.MrCustTypeCode == CommonConstant.CustTypeCompany){
+    else if (this.MrCustTypeCode == CommonConstant.CustTypeCompany) {
       this.isCalculated = true;
     }
     this.route.queryParams.subscribe(params => {
-    if (params["Page"] != null) {
-      this.Page = params["Page"];
-    }
-  });
+      if (params["Page"] != null) {
+        this.Page = params["Page"];
+      }
+    });
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    console.log("aaaa")
+    this.attrGroup = this.MrCustTypeCode==CommonConstant.CustTypeCompany ? CommonConstant.AttrGroupCustCompanyFinData : CommonConstant.AttrGroupCustPersonalFinData;
+ 
     var datePipe = new DatePipe("en-US");
     if (this.MrCustTypeCode == CommonConstant.CustTypePersonal) {
       var custPersonalData;
@@ -106,10 +120,10 @@ export class CustFinDataTabComponent implements OnInit {
       custPersonal.CustId = this.CustId;
       this.httpClient.post(URLConstant.GetCustPersonalbyCustId, custPersonal).pipe(
         map((response: CustPersonalObj) => {
-          if(!response || response.MrMaritalStatCode == null){
+          if (!response || response.MrMaritalStatCode == null) {
             this.mrMaritalStatCode = CommonConstant.MR_MARITAL_STAT_CODE_SINGLE;
           }
-          else{
+          else {
             this.mrMaritalStatCode = response.MrMaritalStatCode;
           }
           custPersonalData = response;
@@ -147,6 +161,9 @@ export class CustFinDataTabComponent implements OnInit {
           this.sourceOfIncomeList = sourceIncome;
         }
       );
+
+      this.bindFinancialAttribute();
+
     }
     else if (this.MrCustTypeCode == CommonConstant.CustTypeCompany) {
       var custCompanyData;
@@ -196,6 +213,9 @@ export class CustFinDataTabComponent implements OnInit {
           });
         }
       );
+
+      this.bindFinancialAttribute();
+
     }
   }
 
@@ -213,10 +233,10 @@ export class CustFinDataTabComponent implements OnInit {
       var otherMonthlyInstAmt = formData.OtherMonthlyInstAmt == "" ? 0 : parseInt(this.currencyToNumber(formData.OtherMonthlyInstAmt.toString()));
       var totalAmt = 0;
 
-      if(formData.IsJoinIncome){
+      if (formData.IsJoinIncome) {
         totalAmt = monthlyIncomeAmt + spouseMonthlyIncomeAmt + totalIncomeAmt + nettIncomeAmt + nettProfitMonthlyAmt + otherIncomeAmt;
       }
-      else{
+      else {
         totalAmt = monthlyIncomeAmt + totalIncomeAmt + nettIncomeAmt + nettProfitMonthlyAmt + otherIncomeAmt;
       }
       var netIncomeAmt = totalAmt - (monthlyExpenseAmt + monthlyInstallmentAmt + otherMonthlyInstAmt);
@@ -230,11 +250,11 @@ export class CustFinDataTabComponent implements OnInit {
     }
   }
 
-  currencyFormatter(value: string){
+  currencyFormatter(value: string) {
     return value.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  currencyToNumber(value: string){
+  currencyToNumber(value: string) {
     return value.replace(/,/g, "");
   }
 
@@ -290,16 +310,17 @@ export class CustFinDataTabComponent implements OnInit {
   // }
 
   next() {
+    if (this.RefAttrForm.valid != true) return;
     var response;
     var url;
 
     if (this.MrCustTypeCode == CommonConstant.CustTypePersonal) {
       var tempResponse = this.CustPersonalFinDataForm.value;
-      if(this.mrMaritalStatCode != CommonConstant.MR_MARITAL_STAT_CODE_MARRIED){
+      if (this.mrMaritalStatCode != CommonConstant.MR_MARITAL_STAT_CODE_MARRIED) {
         tempResponse.SpouseMonthlyIncomeAmt = 0;
       }
-      else{
-        if(tempResponse.SpouseMonthlyIncomeAmt = ""){
+      else {
+        if (tempResponse.SpouseMonthlyIncomeAmt = "") {
           tempResponse.SpouseMonthlyIncomeAmt = 0;
         }
       }
@@ -314,14 +335,14 @@ export class CustFinDataTabComponent implements OnInit {
       response.OtherIncomeAmt = this.currencyToNumber(response.OtherIncomeAmt.toString());
       response.OtherMonthlyInstAmt = this.currencyToNumber(response.OtherMonthlyInstAmt.toString());
 
-      if(response.CustPersonalFinDataId > 0){
+      if (response.CustPersonalFinDataId > 0) {
         url = URLConstant.EditCustPersonalFinData;
       }
-      else{
+      else {
         url = URLConstant.AddCustPersonalFinData
       }
     }
-    else if (this.MrCustTypeCode ==  CommonConstant.CustTypeCompany) {
+    else if (this.MrCustTypeCode == CommonConstant.CustTypeCompany) {
       response = this.CustCompanyFinDataForm.value;
       response.GrossMonthlyIncomeAmt = this.currencyToNumber(response.GrossMonthlyIncomeAmt.toString());
       response.GrossProfitAmt = this.currencyToNumber(response.GrossProfitAmt.toString());
@@ -337,37 +358,211 @@ export class CustFinDataTabComponent implements OnInit {
       response.LongTemrLiablts = this.currencyToNumber(response.LongTemrLiablts.toString());
       response.ShareholderEquity = this.currencyToNumber(response.ShareholderEquity.toString());
 
-      if(response.CustCompanyFinDataId > 0){
+      if (response.CustCompanyFinDataId > 0) {
         url = URLConstant.EditCustCompanyFinData;
       }
-      else{
+      else {
         url = URLConstant.AddCustCompanyFinData;
       }
     }
 
     if (this.isCalculated) {
-      if(response.SpouseMonthlyIncomeAmt == ""){
+      if (response.SpouseMonthlyIncomeAmt == "") {
         response.SpouseMonthlyIncomeAmt = this.spouseMonthlyIncomeAmt;
       }
       this.httpClient.post(url, response).subscribe(
         (response) => {
           this.toastr.successMessage(response["Message"]);
-          if(this.MrCustTypeCode == CommonConstant.CustTypePersonal){  
-            // if (this.Page != null) {
-            //   this.router.navigate(["/Customer/EditMainData/Paging"]);
-            // } else {
-            //   this.router.navigate(["/Customer/Paging"]);
-            // }
-            this.outputTab.emit({ stepMode: "next"});
+
+          var formValue = this.RefAttrForm.value;
+          var custAttrRequest = new Array<Object>();
+          var urlAttr = URLConstant.AddEditListCustAttrContent;
+          if (Object.keys(formValue).length > 0 && formValue.constructor === Object) {
+            for (const key in formValue) {
+              if (formValue[key]["AttrValue"] != null) {
+                var custAttr = {
+                  CustAttrContentId: formValue[key]["CustAttrContentId"],
+                  CustId: this.CustId,
+                  RefAttrId: formValue[key]["RefAttrId"],
+                  AttrValue: formValue[key]["AttrValue"],
+                  AttrGroup: this.attrGroup
+                };
+                custAttrRequest.push(custAttr);
+              }
+            }
+            this.httpClient.post(urlAttr, { CustAttrContentObjs: custAttrRequest }).pipe(first()).subscribe(
+              (response) => {
+                this.toastr.successMessage(response["Message"]);
+                this.outputTab.emit({ stepMode: "next" });
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
           }
-          else if(this.MrCustTypeCode == CommonConstant.CustTypeCompany){
-            this.outputTab.emit({ stepMode: "next"});
-          }
+          // if(this.MrCustTypeCode == CommonConstant.CustTypePersonal){  
+          //   // if (this.Page != null) {
+          //   //   this.router.navigate(["/Customer/EditMainData/Paging"]);
+          //   // } else {
+          //   //   this.router.navigate(["/Customer/Paging"]);
+          //   // }
+          //   this.outputTab.emit({ stepMode: "next"});
+          // }
+          // else if(this.MrCustTypeCode == CommonConstant.CustTypeCompany){
+          //   this.outputTab.emit({ stepMode: "next"});
+          // }  
         }
       );
     }
     else {
       this.toastr.warningMessage("Please Calculate First");
     }
+  }
+  SplitAttrListValue(value, attrCode) {
+    return value.split(";");
+  }
+  getLookUp(e, AttrCode) {
+    console.log(this.RefAttrForm)
+    this.RefAttrForm["controls"][AttrCode].patchValue({
+      AttrValue: e.MasterCode
+    });
+    console.log(this.RefAttrForm)
+
+  }
+  async bindFinancialAttribute() {
+    var AttrContent = {
+      CustId: this.CustId,
+      AttrGroup: this.attrGroup
+    };
+
+    await this.httpClient.post(URLConstant.GetListCustAttrContentByCustIdAndAttrGroup, AttrContent).toPromise().then(
+      (response) => {
+        console.log(response)
+        this.ListAttrContent = response[CommonConstant.ReturnObj]
+        if (this.ListAttrContent.length < 1) {
+          var custGrp = {
+            AttrGroup: this.attrGroup
+          };
+          this.httpClient.post(URLConstant.GetListActiveRefAttrByAttrGroup, custGrp).subscribe(
+            (response: any) => {
+              var parentFormGroup = new Object();
+              this.RefAttrList = response[CommonConstant.ReturnObj];
+
+              let _temp = {};
+              for (const refAttr of this.RefAttrList) {
+
+                var formGroupObject = new Object();
+                formGroupObject["CustAttrContentId"] = [0];
+                formGroupObject["RefAttrId"] = [refAttr["RefAttrId"], [Validators.required]];
+                if (refAttr["AttrInputType"] == 'L') {
+                  var temp = refAttr["AttrValue"].split(";");
+                  formGroupObject["AttrValue"] = [temp[0], [Validators.required]];
+                } else {
+                  formGroupObject["AttrValue"] = [refAttr["AttrValue"], [Validators.required]];
+                }
+                parentFormGroup[refAttr["AttrCode"]] = this.fb.group(formGroupObject);
+
+                if (refAttr["AttrInputType"] == 'RM') {
+                  _temp[refAttr["AttrCode"]] = new InputLookupObj();
+                  _temp[refAttr["AttrCode"]].urlJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                  _temp[refAttr["AttrCode"]].urlQryPaging = URLConstant.GetPagingObjectBySQL;
+                  _temp[refAttr["AttrCode"]].urlEnviPaging = environment.FoundationR3Url;
+                  _temp[refAttr["AttrCode"]].pagingJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                  _temp[refAttr["AttrCode"]].genericJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+
+                  var arrAddCrit = new Array();
+                  var critAssetObj = new CriteriaObj();
+                  critAssetObj.DataType = 'text';
+                  critAssetObj.restriction = AdInsConstant.RestrictionEq;
+                  critAssetObj.propName = 'REF_MASTER_TYPE_CODE';
+                  critAssetObj.value = refAttr.AttrValue;
+                  arrAddCrit.push(critAssetObj);
+                  _temp[refAttr["AttrCode"]].addCritInput = arrAddCrit;
+                }
+
+              }
+              this.ListInputLookUpObj.push(_temp);
+              this.RefAttrForm = this.fb.group(parentFormGroup);
+              this.isRefAttrFormReady = true;
+
+            }
+          );
+        }
+        else {
+          this.isAdd = false;
+          var parentFormGroup = new Object();
+          let _temp = {};
+
+          var custGrp = {
+            AttrGroup: this.attrGroup
+          };
+          this.httpClient.post(URLConstant.GetListActiveRefAttrByAttrGroup, custGrp).subscribe(
+            (response: any) => {
+              this.RefAttrList = response[CommonConstant.ReturnObj];
+              for (const refAttr of this.RefAttrList) {
+                var item = this.ListAttrContent.find(x => x.RefAttrId == refAttr.RefAttrId);
+                if (item == undefined) {
+                  var formGroupObject = new Object();
+                  formGroupObject["CustAttrContentId"] = [0];
+                  formGroupObject["RefAttrId"] = [refAttr["RefAttrId"], [Validators.required]];
+                  if (refAttr["AttrInputType"] == 'L') {
+                    var temp = refAttr["AttrValue"].split(";");
+                    formGroupObject["AttrValue"] = [temp[0], [Validators.required]];
+                  } else {
+                    formGroupObject["AttrValue"] = [refAttr["AttrValue"], [Validators.required]];
+                  }
+                  parentFormGroup[refAttr["AttrCode"]] = this.fb.group(formGroupObject);
+
+
+                  if (refAttr["AttrInputType"] == 'RM') {
+                    _temp[refAttr["AttrCode"]] = new InputLookupObj();
+                    _temp[refAttr["AttrCode"]].urlJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                    _temp[refAttr["AttrCode"]].urlQryPaging = URLConstant.GetPagingObjectBySQL;
+                    _temp[refAttr["AttrCode"]].urlEnviPaging = environment.FoundationR3Url;
+                    _temp[refAttr["AttrCode"]].pagingJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                    _temp[refAttr["AttrCode"]].genericJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+
+                    var arrAddCrit = new Array();
+                    var critAssetObj = new CriteriaObj();
+                    critAssetObj.DataType = 'text';
+                    critAssetObj.restriction = AdInsConstant.RestrictionEq;
+                    critAssetObj.propName = 'REF_MASTER_TYPE_CODE';
+                    critAssetObj.value = refAttr.AttrValue;
+                    arrAddCrit.push(critAssetObj);
+                    _temp[refAttr["AttrCode"]].addCritInput = arrAddCrit;
+                  }
+
+                } else {
+                  var formGroupObject = new Object();
+                  formGroupObject["CustAttrContentId"] = [item["CustAttrContentId"], [Validators.required]];
+                  formGroupObject["RefAttrId"] = [item["RefAttrId"], [Validators.required]];
+                  formGroupObject["AttrValue"] = [item["AttrValue"], [Validators.required]];
+                  parentFormGroup[item["AttrCode"]] = this.fb.group(formGroupObject);
+
+                  if (item["AttrInputType"] == 'RM') {
+                    _temp[item["AttrCode"]] = new InputLookupObj();
+                    _temp[item["AttrCode"]].urlJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                    _temp[item["AttrCode"]].urlQryPaging = URLConstant.GetPagingObjectBySQL;
+                    _temp[item["AttrCode"]].urlEnviPaging = environment.FoundationR3Url;
+                    _temp[item["AttrCode"]].pagingJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                    _temp[item["AttrCode"]].genericJson = "./assets/uclookup/RefMaster/lookupRefMaster.json";
+                    _temp[item["AttrCode"]].jsonSelect = { Descr: item["Descr"] }
+                    var arrAddCrit = new Array();
+                    var critAssetObj = new CriteriaObj();
+                    critAssetObj.DataType = 'text';
+                    critAssetObj.restriction = AdInsConstant.RestrictionEq;
+                    critAssetObj.propName = 'REF_MASTER_TYPE_CODE';
+                    critAssetObj.value = item.MasterCode;
+                    arrAddCrit.push(critAssetObj);
+                    _temp[item["AttrCode"]].addCritInput = arrAddCrit;
+                  }
+                }
+              }
+              this.ListInputLookUpObj.push(_temp);
+              this.RefAttrForm = this.fb.group(parentFormGroup);
+              this.isRefAttrFormReady = true;
+            });
+        }
+      });
   }
 }
