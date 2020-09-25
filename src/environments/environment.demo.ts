bdee5dd137e34612f795ea5e1bf136a1e5917cda
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-09-25 17:13:06 +0700

    envi demo, url to public

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -6,12 +6,12 @@
 export const environment = {
   production: true,
   navbarColor: 'lightgray',
-  LosURL : 'http://r3web-server.ad-ins.com/LOS_DEMO/',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEMO',
-  WebSocketURL : 'http://r3app-server.ad-ins.com/FOUNDATION_DEMO',
+  LosURL : 'http://r3web.ad-ins.com/LOS_DEMO/',
+  FoundationR3Url: 'http://r3app.ad-ins.com/FOUNDATION_DEMO',
+  WebSocketURL : 'http://r3app.ad-ins.com/FOUNDATION_DEMO',
   Module:"FOU",
-  ApprovalURL : 'http://r3app-server/APPROVAL_DEMO',
-  FoundationR3Web:'http://r3web-server.ad-ins.com/FOUNDATION_DEMO'
+  ApprovalURL : 'http://r3app.ad-ins.com/APPROVAL_DEMO',
+  FoundationR3Web:'http://r3web.ad-ins.com/FOUNDATION_DEMO'
 };
 
 
