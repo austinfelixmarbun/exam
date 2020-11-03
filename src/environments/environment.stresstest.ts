6c003cd676213e44753870f3792b5f761806cbf9
Ardi Gunawan <elnavaro@live.com>
2020-11-03 11:17:04 +0700

    Update Envi Stresstest

--- src/environments/environment.stresstest.ts
+++ src/environments/environment.stresstest.ts
@@ -6,10 +6,10 @@
 export const environment = {
     production: true,
     navbarColor: 'lime',
-    LosURL : 'http://R3STWEBFE.ad-ins.com/LOSFE/',
-    WebSocketURL : 'http://R3STWEBBE.ad-ins.com/FOUNDATIONBE',
-    FoundationR3Url: 'http://R3STWEBBE.ad-ins.com/FOUNDATIONBE',
-    Module:"FOU",
+    LosURL : 'http://R3STWEBFE.ad-ins.com/LOS_FE/',
+    WebSocketURL : 'http://R3STWEBBE.ad-ins.com/FOUNDATION_BE',
+    FoundationR3Url: 'http://R3STWEBBE.ad-ins.com/FOUNDATION_BE',
     ApprovalURL : 'http://r3stengine.ad-ins.com/approval',
-    FoundationR3Web:'http://R3STWEBFE.ad-ins.com/FOUNDATIONFE'
+    FoundationR3Web:'http://R3STWEBFE.ad-ins.com/FOUNDATION_FE',
+    Module:"FOU"
   };
