e4e99e65ed2e5af3e096e39d19931558d95603bc
richard suprayogi <richard.suprayogi@ad-ins.com>
2020-12-28 11:10:36 +0700

    fixing bug urs-los-041

--- src/app/customer/customer-company/customer-company-contact-information/customer-company-contact-information.component.ts
+++ src/app/customer/customer-company/customer-company-contact-information/customer-company-contact-information.component.ts
@@ -362,22 +362,38 @@ export class CustomerCompanyContactInformationComponent implements OnInit {
       }
     );
   }
-  setValidatorPattern(){
+  // setValidatorPattern(){
+  //   let idTypeValue: string;
+
+  //   idTypeValue = this.ContactInformationForm.controls[this.controlNameIdType].value;
+
+  //   if (this.resultPattern != undefined) {
+  //     var result = this.resultPattern.find(x => x.Key == idTypeValue)
+
+  //     if (result != undefined) {
+  //       var pattern = result.Value;
+  //       if (pattern != undefined) {
+  //         this.setValidator(pattern);
+  //       }
+  //     }
+  //   }
+  // }
+
+  setValidatorPattern() {
     let idTypeValue: string;
-
     idTypeValue = this.ContactInformationForm.controls[this.controlNameIdType].value;
-
-    if (this.resultPattern != undefined) {
-      var result = this.resultPattern.find(x => x.Key == idTypeValue)
-
-      if (result != undefined) {
-        var pattern = result.Value;
-        if (pattern != undefined) {
-          this.setValidator(pattern);
+    var pattern: string = '';
+    if (idTypeValue != undefined) {
+      if (this.resultPattern != undefined) {
+        var result = this.resultPattern.find(x => x.Key == idTypeValue)
+        if (result != undefined) {
+          pattern = result.Value;
         }
       }
     }
+    this.setValidator(pattern);
   }
+
   setValidator(pattern: string) {
     if (pattern != undefined) {
       this.ContactInformationForm.controls[this.controlNameIdNo].setValidators(Validators.pattern(pattern));
--- src/app/vendor/vendor-branch/vendor-branch-add-edit/vendor-branch-add-edit.component.ts
+++ src/app/vendor/vendor-branch/vendor-branch-add-edit/vendor-branch-add-edit.component.ts
@@ -856,22 +856,38 @@ export class VendorBranchAddEditComponent implements OnInit {
       }
     );
   }
-  setValidatorPattern(){
+  // setValidatorPattern(){
+  //   let idTypeValue: string;
+
+  //   idTypeValue = this.VendorForm.controls[this.controlNameIdType].value;
+
+  //   if (this.resultPattern != undefined) {
+  //     var result = this.resultPattern.find(x => x.Key == idTypeValue)
+
+  //     if (result != undefined) {
+  //       var pattern = result.Value;
+  //       if (pattern != undefined) {
+  //         this.setValidator(pattern);
+  //       }
+  //     }
+  //   }
+  // }
+
+  setValidatorPattern() {
     let idTypeValue: string;
-
     idTypeValue = this.VendorForm.controls[this.controlNameIdType].value;
-
-    if (this.resultPattern != undefined) {
-      var result = this.resultPattern.find(x => x.Key == idTypeValue)
-
-      if (result != undefined) {
-        var pattern = result.Value;
-        if (pattern != undefined) {
-          this.setValidator(pattern);
+    var pattern: string = '';
+    if (idTypeValue != undefined) {
+      if (this.resultPattern != undefined) {
+        var result = this.resultPattern.find(x => x.Key == idTypeValue)
+        if (result != undefined) {
+          pattern = result.Value;
         }
       }
     }
+    this.setValidator(pattern);
   }
+
   setValidator(pattern: string) {
     if (pattern != undefined) {
       this.VendorForm.controls[this.controlNameIdNo].setValidators(Validators.pattern(pattern));
