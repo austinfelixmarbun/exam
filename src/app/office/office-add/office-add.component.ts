3fad3346318746b9fffb64bc4383df6a571f4326
richard suprayogi <richard.suprayogi@ad-ins.com>
2021-01-07 10:54:45 +0700

    LOSR3 3748:rollback URS-LOS-095 pada absorb produk

--- src/app/office/office-add/office-add.component.ts
+++ src/app/office/office-add/office-add.component.ts
@@ -89,10 +89,6 @@ export class OfficeAddComponent implements OnInit {
   officeTypeUrl: any;
   officeparentId: any;
 
-  resultDataLawCourt: any;
-
-  cbIsNationalCourt: boolean;
-
   OfficeForm = this.fb.group({
     OfficeCode: ['', Validators.required],
     OfficeName: ['', Validators.required],
@@ -122,9 +118,7 @@ export class OfficeAddComponent implements OnInit {
     CntctPersonMobilePhnNo2: ['', [Validators.pattern('^[0-9]+$')]],
     IsActive: false,
     OfficeClose: false,
-    AllowAppCreated: false,
-    IsNationalCourt: false,
-    NationalCourtOffice : ['']
+    AllowAppCreated: false
   })
   InputLookupObj: any;
   addressObj: UcAddressObj;
@@ -156,9 +150,6 @@ export class OfficeAddComponent implements OnInit {
     });
   }
   ngOnInit() {
-    this.cbIsNationalCourt = false;
-    this.IsNationalCourtChange();
-
     this.InputLookupObj = new InputLookupObj();
     this.InputLookupObj.urlJson = "./assets/lookup/lookupOfficeParent.json";
     this.InputLookupObj.urlQryPaging = "/Generic/GetPagingObjectBySQL";
@@ -296,10 +287,7 @@ export class OfficeAddComponent implements OnInit {
             CntctPersonEmail1: this.resultData.CntctPersonEmail1,
             CntctPersonEmail2: this.resultData.CntctPersonEmail2,
             CntctPersonMobilePhnNo1: this.resultData.CntctPersonMobilePhnNo1,
-            CntctPersonMobilePhnNo2: this.resultData.CntctPersonMobilePhnNo2,
-
-            IsNationalCourt: this.resultData.IsNationalCourt,
-            NationalCourtOffice: this.resultData.NationalCourtOffice
+            CntctPersonMobilePhnNo2: this.resultData.CntctPersonMobilePhnNo2
           })
           this.checkType();
           this.addressObj.Addr = this.resultData.OfficeAddr;
@@ -380,18 +368,7 @@ export class OfficeAddComponent implements OnInit {
               }
             })
 
-            this.cbIsNationalCourt = this.resultData.IsNationalCourt;
-
-            if (this.cbIsNationalCourt == true) {
-              this.OfficeForm.controls.NationalCourtOffice.enable()
-              this.OfficeForm.controls.NationalCourtOffice.clearValidators();
-              this.OfficeForm.controls.NationalCourtOffice.setValidators([Validators.required]);
-            } else {
-              this.OfficeForm.controls.NationalCourtOffice.disable()
-              this.OfficeForm.controls.NationalCourtOffice.clearValidators();
-            }
-            this.OfficeForm.controls.NationalCourtOffice.updateValueAndValidity();
-        })
+          })
     }
     this.inputAddressObj = new InputAddressObj();
     this.inputAddressObj.default = this.addressObj;
@@ -414,9 +391,6 @@ export class OfficeAddComponent implements OnInit {
     this.officeObj.IsOfficeClose = this.OfficeForm.value.OfficeClose;
     this.officeObj.CntctPersonName = this.OfficeForm.value.CntctPersonName;
     this.officeObj.CntctPersonJobTitle = this.OfficeForm.value.CntctPersonJobTitle;
-
-    this.officeObj.IsNationalCourt = this.OfficeForm.value.IsNationalCourt;
-    this.officeObj.NationalCourtOffice = this.OfficeForm.value.NationalCourtOffice;
 
     if (this.OfficeForm.controls.OfficeType.value == CommonConstant.HeadOffice) {
       this.officeObj.ParentId = null;
@@ -521,19 +495,5 @@ export class OfficeAddComponent implements OnInit {
     // });
     // this.InputLookupObj.nameSelect = ev.zipcode;
     // this.InputLookupObj.idSelect = ev.zipcode;
-  }
-
-  IsNationalCourtChange(){
-    if(this.cbIsNationalCourt == true)
-    {
-      this.OfficeForm.controls.NationalCourtOffice.enable()
-      this.OfficeForm.controls.NationalCourtOffice.clearValidators();
-      this.OfficeForm.controls.NationalCourtOffice.setValidators([Validators.required]);
-    }else{
-      this.OfficeForm.controls.NationalCourtOffice.setValue("");
-      this.OfficeForm.controls.NationalCourtOffice.disable()
-      this.OfficeForm.controls.NationalCourtOffice.clearValidators();
-    }
-    this.OfficeForm.controls.NationalCourtOffice.updateValueAndValidity();
   }
 }
