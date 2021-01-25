e1089f3e5cd2b42f494d4bca36914f7e6bd51209
Andry Gunawan <andry.gunawan@ad-ins.com>
2021-01-25 16:58:31 +0700

    tambah DMSUrl di environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,6 +16,7 @@ export const environment = {
   FoundationR3ControllerUrl: "http://r3impl-appsvr.ad-ins.com/FOUDSF",
   ApprovalR3Url: "http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF",
   ConfinsR2Url: "http://r3impl-appsvr.ad-ins.com/CONFINSR2/api/",
+  DMSUrl: "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
   ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
   ChipperKeyCookie: "AdInsFOU12345678" // 256 bit atau 16 karakter & harus sama dengan BE
 };
