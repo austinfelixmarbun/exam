381abe74e64649236d02b32189b65a0d62f69f69
vincent.andreas <vincent.andreas89@gmail.com>
2021-02-22 19:08:00 +0700

    SITDSFCF-221

--- src/app/customer/cust-group-tab/cust-group-tab-detail/cust-group-tab-detail.component.ts
+++ src/app/customer/cust-group-tab/cust-group-tab-detail/cust-group-tab-detail.component.ts
@@ -52,7 +52,7 @@ export class CustGroupTabDetailComponent implements OnInit {
     var criteriaObj;
     var refMasterRelationship = new RefMasterObj();
     this.CustGrpForm.patchValue({
-      MemberCustId: this.CustId
+      CustId: this.CustId
     });
     
     if(this.MrCustTypeCode == CommonConstant.CustTypePersonal){
@@ -137,7 +137,7 @@ export class CustGroupTabDetailComponent implements OnInit {
     this.isCustPicked = true;
     var refMasterRelationship = new RefMasterObj();
     this.CustGrpForm.patchValue({
-      CustId: e.custId,
+      MemberCustId: e.custId,
       CustNo: e.custNo,
       CustName: e.custName
     });
@@ -163,7 +163,7 @@ export class CustGroupTabDetailComponent implements OnInit {
     this.isCustPicked = true;
     var refMasterRelationship = new RefMasterObj();
     this.CustGrpForm.patchValue({
-      CustId: e.custId,
+      MemberCustId: e.custId,
       CustNo: e.custNo,
       CustName: e.custName
     });
