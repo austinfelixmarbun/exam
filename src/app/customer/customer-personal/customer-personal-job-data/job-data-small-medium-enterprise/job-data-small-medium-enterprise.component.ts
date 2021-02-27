9d2338fc4ac04087990aef96c29622b47ffb6c32
Randy Khengdy <randy.khengdy@ad-ins.com>
2021-02-27 15:14:52 +0700

    Job data small medium

--- src/app/customer/customer-personal/customer-personal-job-data/job-data-small-medium-enterprise/job-data-small-medium-enterprise.component.ts
+++ src/app/customer/customer-personal/customer-personal-job-data/job-data-small-medium-enterprise/job-data-small-medium-enterprise.component.ts
@@ -160,6 +160,9 @@ export class JobDataSmeComponent implements OnInit {
     this.inputAddressObjForOthBiz.title = "Other Business Address";
     this.inputAddressObjForOthBiz.showOwnership = true;
 
+ 
+  
+
     this.inputPreviousAddressObj = new InputAddressObj();
     this.inputPreviousAddressObj.showSubsection = false;
     this.inputPreviousAddressObj.isRequired = false;
@@ -171,10 +174,12 @@ export class JobDataSmeComponent implements OnInit {
     this.businessDtMin.setDate(this.businessDtMin.getDate() - 1);
     this.inputJobAddressObj = new InputFieldObj();
     this.inputJobAddressObj.inputLookupObj = new InputLookupObj();
+    this.inputJobAddressObj.inputLookupObj.isRequired = false;
+    this.inputAddressObjForOthBiz.inputField = this.inputJobAddressObj;
     this.inputOtherAddressObj = new InputFieldObj();
     this.inputOtherAddressObj.inputLookupObj = new InputLookupObj();
     this.inputOtherAddressObj.inputLookupObj.isRequired = false;
-    
+    this.inputPreviousAddressObj.inputField = this.inputOtherAddressObj;
     this.inputPreJobAddressObj =  new InputFieldObj();
     this.inputPreJobAddressObj.inputLookupObj = new InputLookupObj();
     this.inputPreJobAddressObj.inputLookupObj.isRequired = false;
