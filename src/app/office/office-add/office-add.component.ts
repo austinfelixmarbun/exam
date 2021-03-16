1191a33482c900697edd7c4fb5e3e941bf90faff
muhammad.sa <muhammad.sa@ad-ins.com>
2021-03-16 12:12:32 +0700

    FIx Bug 4179: add crit office parent tidak menunjuk diri sendiri

--- src/app/office/office-add/office-add.component.ts
+++ src/app/office/office-add/office-add.component.ts
@@ -34,7 +34,7 @@ export class OfficeAddComponent implements OnInit {
   mrKonvenSyariah = 'KON';
   isDisabledState: boolean = false;
   isHO: boolean = true;
-  RefOfficeId: any;
+  RefOfficeId: number;
   allOfficeType: any;
   allOfficeClass: any;
   allRefOrg: any;
@@ -102,16 +102,6 @@ export class OfficeAddComponent implements OnInit {
     HolidayScheme: ['', Validators.required],
     WorkingHourScheme: ['', Validators.required],
     MrCenterGrpTypeCode: ['', Validators.required],
-    // PhnArea1: ['', [Validators.required, Validators.max(4)]],
-    // Phn1: ['',Validators.required],
-    // PhnExt1: ['',[Validators.required, Validators.max(4)]],
-    // PhnArea2:  ['',Validators.max(4)],
-    // Phn2: [''],
-    // PhnExt2: ['',Validators.max(4)],
-    // PhnArea3: ['',Validators.max(4)],
-    // Phn3:  [''],
-    // PhnExt3:  ['',Validators.max(4)],
-    // Fax:  ['',Validators.max(4)],
     CntctPersonName: ['', Validators.required],
     CntctPersonJobTitle: ['', Validators.required],
     CntctPersonEmail1: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
@@ -163,6 +153,17 @@ export class OfficeAddComponent implements OnInit {
         environment: environment.FoundationR3Url
       }
     ];
+    this.InputLookupObj.addCritInput = new Array();
+
+    if(this.RefOfficeId != undefined && this.RefOfficeId != 0){
+      var critRefOfficeIdObj = new CriteriaObj();
+      critRefOfficeIdObj.restriction = AdInsConstant.RestrictionNeq;
+      critRefOfficeIdObj.propName = 'A.REF_OFFICE_ID';
+      critRefOfficeIdObj.value = this.RefOfficeId.toString();
+      console.log("testing...");
+      console.log(this.InputLookupObj);
+      this.InputLookupObj.addCritInput.push(critRefOfficeIdObj);
+    }
 
     this.refMasterObj = new RefMasterObj();
     this.refMasterObj.RefMasterTypeCode = CommonConstant.RefMasterTypeCodeOfficeClass;
@@ -190,9 +191,9 @@ export class OfficeAddComponent implements OnInit {
             critObj.listValue.push(element.Key);
           });
 
-          this.arrCrit.push(critObj);
-          this.InputLookupObj.addCritInput = this.arrCrit;
+          this.InputLookupObj.addCritInput.push(critObj);
         }
+        this.InputLookupObj.isReady = true;
 
       });
 
@@ -488,13 +489,5 @@ export class OfficeAddComponent implements OnInit {
     this.OfficeForm.patchValue({
       OfficeParent: ev.RefOfficeId
     })
-    // this.CustForm.patchValue({
-    //   AreaCode2: ev.AreaCode2,
-    //   AreaCode1: ev.AreaCode1,
-    //   City: ev.City,
-    //   ZipCode: ev.ZipCode
-    // });
-    // this.InputLookupObj.nameSelect = ev.zipcode;
-    // this.InputLookupObj.idSelect = ev.zipcode;
   }
 }
