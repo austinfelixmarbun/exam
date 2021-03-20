5aa4a2d9260c658078214965ea37721074ed063f
richard suprayogi <richard.suprayogi@ad-ins.com>
2021-03-20 13:06:44 +0700

    rollback

--- src/app/product/product-HO/product-ho-approval/product-ho-approval.component.ts
+++ src/app/product/product-HO/product-ho-approval/product-ho-approval.component.ts
@@ -87,7 +87,7 @@ export class ProductHOApprovalComponent implements OnInit {
         this.toastr.warningMessage(ExceptionConstant.NOT_ELIGIBLE_FOR_TAKE_BACK);
       } else {
         ApvReqObj.TaskId = ev.RowObj.TaskId;
-        ApvReqObj.UsernameMemberId = ev.RowObj.MainUsernameMemberId;
+        // ApvReqObj.UsernameMemberId = ev.RowObj.MainUsernameMemberId;
         this.httpClient.post(AdInsConstant.ApvTakeBackTaskUrl, ApvReqObj).subscribe(
           (response) => {
             this.toastr.successMessage(response["Message"]);
