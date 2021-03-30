ae5c868662c7fc7c295951e103b8ce571b9cfd33
Reynard.wf <reynard.wf@ad-ins.com>
2021-03-30 14:37:18 +0700

    genericDTO fix

--- src/app/asset/asset-accessory/asset-accessory-add-edit/asset-accessory-add-edit.component.ts
+++ src/app/asset/asset-accessory/asset-accessory-add-edit/asset-accessory-add-edit.component.ts
@@ -41,6 +41,7 @@ export class AssetAccessoryAddEditComponent implements OnInit {
     });
   }
   ngOnInit() {
+    console.log("reyn");
     this.http.post(URLConstant.GetAssetTypeById, {Id: this.AssetTypeId }).subscribe(
       (response) => {
         this.assetTypeName = response['AssetTypeName'];
@@ -51,7 +52,7 @@ export class AssetAccessoryAddEditComponent implements OnInit {
       acObj.AssetAccessoryId = this.AssetAccessoryId;
       acObj.AssetTypeId = this.AssetTypeId;
       this.AssetAccessoryForm.controls.AssetAccessoryCode.disable();
-      let obj = {Code: this.acObj.AssetAccessoryId};
+      let obj = {Id: this.acObj.AssetAccessoryId};
 
       this.http.post<AssetAccessoryObj>(URLConstant.GetAssetAccessorybyAssetAccessoryId, obj).subscribe(
         (response) => {
