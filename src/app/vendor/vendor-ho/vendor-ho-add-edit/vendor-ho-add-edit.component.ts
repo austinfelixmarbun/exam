d678e5248ea371b02e41db0bd69bbe7e951db2ab
Justine.rusli <justine.rusli@ad-ins.com>
2021-03-31 14:19:01 +0700

    4335 - Sweeping SupplierBranch

--- src/app/vendor/vendor-ho/vendor-ho-add-edit/vendor-ho-add-edit.component.ts
+++ src/app/vendor/vendor-ho/vendor-ho-add-edit/vendor-ho-add-edit.component.ts
@@ -103,7 +103,7 @@ export class VendorHoAddEditComponent implements OnInit {
         this.HoTitle = "Supplier Holding ";
         break;
       case CommonConstant.SUPPLIER:
-        this.HoTitle = "Supplier Branch ";
+        this.HoTitle = "Supplier ";
         break;
       case CommonConstant.SURVEYOR_HO:
         this.HoTitle = "Surveyor HO ";
