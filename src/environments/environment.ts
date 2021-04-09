2cde0c0deea05dfdce1c409997f8ee6cd40595b1
Daniel.da <daniel.da@ad-ins.com>
2021-04-09 13:01:24 +0700

    1. Penambahan validasi add-edit filing 2. Perbaikan validasi cabinet information mandatory

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   DMSUrl: "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
-  // FoundationR3Url: 'https://localhost:5001',
+  //FoundationR3Url: 'https://localhost:5001',
   losR3Web: 'http://r3web-server/LOSR3',
   lmsWeb: 'http://r3impl-websvr.ad-ins.com/LMS',
   Module:"FOU",
