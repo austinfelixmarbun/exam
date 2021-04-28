24c88d56d8ac070346fc6ec52aff55fb7e5a5245
Marvin Sutanto <marvin.sutanto@ad-ins.com>
2021-04-28 10:11:51 +0700

    fix double obj

--- src/app/shared/model/Response/Generic/GenericObj.Model.ts
+++ src/app/shared/model/Response/Generic/GenericObj.Model.ts
@@ -10,12 +10,3 @@ export class GenericObj{
         this.RowVersion = "";
     }
 }
-
-export class GenericObj{
-    Id: number;
-    Code: string;
-    TrxNo: string;
-    RowVersion: string;
-    constructor(){        
-    }
-}
