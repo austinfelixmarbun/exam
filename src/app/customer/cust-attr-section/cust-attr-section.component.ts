3965138524eddf2ac3c03b6cc3ba6b964e892ada
Marvin Sutanto <marvin.sutanto@ad-ins.com>
2021-04-28 10:11:42 +0700

    save lbpp debt grp & biz scl menjadi id

--- src/app/customer/cust-attr-section/cust-attr-section.component.ts
+++ src/app/customer/cust-attr-section/cust-attr-section.component.ts
@@ -46,10 +46,10 @@ export class CustAttrSectionComponent implements OnInit {
   }
 
   OtherInformationForm = this.fb.group({
-    LbppmsDebtGrpCode: ['', [Validators.required]],
+    LbppmsDebtGrpId: ['', [Validators.required]],
     LbppmsCntrprtId: ['', [Validators.required]],
     LbppmsBizSustainId: ['', [Validators.required]],
-    LbppmsBizSclCode: ['', [Validators.required]]
+    LbppmsBizSclId: ['', [Validators.required]]
   }); 
   inputDebitorGroupLookupObj : InputLookupObj;
   inputDebitorBusinessScaleLookupObj: InputLookupObj;
@@ -104,10 +104,10 @@ export class CustAttrSectionComponent implements OnInit {
         this.inputSustaianableFinancialBusinessLookupObj.jsonSelect = {Descr: this.CustOtherInfo.LbppmsBizSustainDescr};
 
         this.OtherInformationForm.patchValue({
-          LbppmsDebtGrpCode:   this.CustOtherInfo.LbppmsDebtGrpCode,
+          LbppmsDebtGrpId:   this.CustOtherInfo.LbppmsDebtGrpId,
           LbppmsCntrprtId: this.CustOtherInfo.LbppmsCntrprtId,
           LbppmsBizSustainId: this.CustOtherInfo.LbppmsBizSustainId,
-          LbppmsBizSclCode: this.CustOtherInfo.LbppmsBizSclCode
+          LbppmsBizSclId: this.CustOtherInfo.LbppmsBizSclId
         });
       }
       this.isLookupReady = true;
@@ -184,13 +184,13 @@ export class CustAttrSectionComponent implements OnInit {
 
   getLookupDebitorGroup(e){
     this.OtherInformationForm.patchValue({
-      LbppmsDebtGrpCode: e.LbppmsDebtGrpCode
+      LbppmsDebtGrpId: e.LbppmsDebtGrpId
     }); 
   }
 
   getLookupDebitorBusinessScale(e){
     this.OtherInformationForm.patchValue({
-      LbppmsBizSclCode: e.LbppmsBizSclCode
+      LbppmsBizSclId: e.LbppmsBizSclId
     }); 
   }
   getLookupCounterpartCategory(e){
