e59f39a0b47c5a0061f7db4418ae600d712c5b39
Darren <darren@ad-ins.com>
2021-05-06 17:16:38 +0700

    PBI 4630: Sweep DTO, ganti req jadi obj

--- src/app/customer/cust-attr-list/cust-attr-list.component.ts
+++ src/app/customer/cust-attr-list/cust-attr-list.component.ts
@@ -11,6 +11,7 @@ import { CriteriaObj } from 'app/shared/model/CriteriaObj.Model';
 import { InputLookupObj } from 'app/shared/model/InputLookupObj.Model';
 import { RefAttr } from 'app/shared/model/RefAttr.Model';
 import { ReqRefMasterByTypeCodeAndMasterCodeObj } from 'app/shared/model/RefMaster/ReqRefMasterByTypeCodeAndMasterCodeObj.Model';
+import { ReqCustAttrContentByCustIdAndAttrGroupObj } from 'app/shared/model/Request/CustAttrContent/ReqCustAttrContentByCustIdAndAttrGroupObj.model';
 import { ReqRefAttrByAttrGroupObj } from 'app/shared/model/Request/RefAttr/ReqRefAttrByAttrGroupObj.model';
 import { environment } from 'environments/environment';
 
@@ -42,13 +43,17 @@ export class CustAttrListComponent implements OnInit {
   ListAttrContent: any;
   tempLookup = {};
   RefAttrList:  Array<RefAttr> = new Array<RefAttr>();
+  ReqByIdAndAttrObj: ReqCustAttrContentByCustIdAndAttrGroupObj = new ReqCustAttrContentByCustIdAndAttrGroupObj();
   ListInputLookUpObj = new Array();
   isFormReady: boolean = false;
   AttrContent: AttrContent;
   async ngOnInit() {
     let custGrp: ReqRefAttrByAttrGroupObj = new ReqRefAttrByAttrGroupObj();
     custGrp.AttrGroup = this.attrGroup;
-    await this.httpClient.post<Array<AttrContent>>(URLConstant.GetListCustAttrContentByCustIdAndAttrGroup, { CustId: this.CustId, AttrGroup: this.attrGroup }).toPromise().then(
+
+    this.ReqByIdAndAttrObj.CustId = this.CustId;
+    this.ReqByIdAndAttrObj.AttrGroup = this.attrGroup;
+    await this.httpClient.post<Array<AttrContent>>(URLConstant.GetListCustAttrContentByCustIdAndAttrGroup, this.ReqByIdAndAttrObj).toPromise().then(
       (response) => {
         this.ListAttrContent = response[CommonConstant.ReturnObj]
         let parentFormGroup = new Object();
