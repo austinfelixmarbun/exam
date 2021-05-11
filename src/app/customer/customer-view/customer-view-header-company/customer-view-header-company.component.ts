5b4809f81dfa206842b6163fd05dcef4fe8210fe
Darren <darren@ad-ins.com>
2021-05-11 16:21:23 +0700

    PBI 3258: buang2in yg gakepake

--- src/app/customer/customer-view/customer-view-header-company/customer-view-header-company.component.ts
+++ src/app/customer/customer-view/customer-view-header-company/customer-view-header-company.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CustObj } from 'app/shared/model/CustObj.Model';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { CustCompanyObj } from 'app/shared/model/CustCompanyObj.Model';
-import { environment } from 'environments/environment';
-import { URLConstant } from 'app/shared/constant/URLConstant';
-import { GenericObj } from 'app/shared/model/Generic/GenericObj.Model';
 import { UcViewGenericObj } from 'app/shared/model/UcViewGenericObj.model';
 
 @Component({
--- src/app/customer/customer-view/customer-view-header-personal/customer-view-header-personal.component.ts
+++ src/app/customer/customer-view/customer-view-header-personal/customer-view-header-personal.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { CustObj } from 'app/shared/model/CustObj.Model';
-import { AdInsConstant } from 'app/shared/AdInstConstant';
-import { CustPersonalObj } from 'app/shared/model/CustPersonalObj.Model';
-import { environment } from 'environments/environment';
-import { URLConstant } from 'app/shared/constant/URLConstant';
 import { UcViewGenericObj } from 'app/shared/model/UcViewGenericObj.model';
 
 @Component({
