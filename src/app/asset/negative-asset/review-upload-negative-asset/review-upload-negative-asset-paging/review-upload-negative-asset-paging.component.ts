007d2f9d8f1869cea14c96519d3d55f82400ad92
Darren <darren@ad-ins.com>
2021-05-28 16:54:48 +0700

    Bug 4826: sweep Uc Paging Envi (Asset)

--- src/app/asset/negative-asset/review-upload-negative-asset/review-upload-negative-asset-paging/review-upload-negative-asset-paging.component.ts
+++ src/app/asset/negative-asset/review-upload-negative-asset/review-upload-negative-asset-paging/review-upload-negative-asset-paging.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UcPagingObj } from 'app/shared/model/UcPagingObj.Model';
-import { environment } from 'environments/environment';
 import { WorkflowApiObj } from 'app/shared/model/WorkflowApiObj.Model';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 import { Router } from '@angular/router';
@@ -21,12 +20,6 @@ export class ReviewUploadNegativeAssetPagingComponent implements OnInit {
   ngOnInit() {
     this.inputPagingObj._url = "./assets/ucpaging/searchReviewUploadNegativeAsset.json";
     this.inputPagingObj.pagingJson = "./assets/ucpaging/searchReviewUploadNegativeAsset.json";
-    this.inputPagingObj.ddlEnvironments = [
-      {
-        name: "UMH.OFFICE_CODE",
-        environment: environment.FoundationR3Url
-      }
-    ];
   }
   cancel(ev) {
     var wfObj = new WorkflowApiObj();
