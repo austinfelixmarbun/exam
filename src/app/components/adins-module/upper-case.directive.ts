635201b6a23c223982baeda310846cf41858bd02
Edward Bala Yansen <edward.by@ad-ins.com>
2021-07-22 10:37:28 +0700

    matiin uppercase sementara

--- src/app/components/adins-module/upper-case.directive.ts
+++ src/app/components/adins-module/upper-case.directive.ts
@@ -1,14 +1,20 @@
 import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: 'input[type=text]',
+  selector: 'input[type=text], input',
 })
 export class UpperCaseDirective {
 
   @Input("IsUpperCase") IsUpperCase: boolean = true;
 
   @HostListener('input', ['$event']) onInputChange($event) {
-    if(this.IsUpperCase) $event.target.value = $event.target.value.toUpperCase();
-    console.log($event.target.value);
+    // console.log($event);
+    // console.log($event.target.value);
+    // if(this.IsUpperCase) {
+    //   console.log($event.data);
+    //   // $event.data = $event.data.toUpperCase();
+    //   $event.target.value = $event.target.value.toUpperCase();
+    // }
+    // console.log($event.target.value);
   }
 }
