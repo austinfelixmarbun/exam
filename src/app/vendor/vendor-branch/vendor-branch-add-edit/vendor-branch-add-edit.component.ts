f28f98a84fdef4e95877c0c7a7bf5cfd61ac44d9
Vincensius Adriel <adrielvincensius@gmail.com>
2021-07-27 17:25:06 +0700

    remove required

--- src/app/vendor/vendor-branch/vendor-branch-add-edit/vendor-branch-add-edit.component.ts
+++ src/app/vendor/vendor-branch/vendor-branch-add-edit/vendor-branch-add-edit.component.ts
@@ -107,7 +107,7 @@ export class VendorBranchAddEditComponent implements OnInit {
     ReservedField2: [''], //Maximum Task load
     ReservedField3: [''],//Supplier calc up method
     ReservedField4: [''], //Supplier Class
-    ReservedField5: ['', [Validators.required]], //BPKBAging
+    ReservedField5: [''], //BPKBAging
     ReservedField6: [''], //DaysPAfterGolive
     ReservedField9: [''], //ASSGMNT_TYPE tele, field
     MrTaxCalcMethodCode: ['', Validators.required],
