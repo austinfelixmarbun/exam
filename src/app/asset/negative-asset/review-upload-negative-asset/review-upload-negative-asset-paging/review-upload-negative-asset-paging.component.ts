4497ca50a57bedeb72f63db0fd81e2e3379d84f5
calvin <calvin.cen@ad-ins.com>
2021-08-03 15:48:36 +0700

    Cekin Fixing Paging Role Code & Upload NAS

--- src/app/asset/negative-asset/review-upload-negative-asset/review-upload-negative-asset-paging/review-upload-negative-asset-paging.component.ts
+++ src/app/asset/negative-asset/review-upload-negative-asset/review-upload-negative-asset-paging/review-upload-negative-asset-paging.component.ts
@@ -38,7 +38,7 @@ export class ReviewUploadNegativeAssetPagingComponent implements OnInit {
       this.requestTaskModel.OfficeCode = UserAccess[CommonConstant.OFFICE_CODE];
       this.requestTaskModel.TaskDefinitionKey = CommonConstant.WfUploadNegativeAssetReview;
       this.requestTaskModel.RoleCode = UserAccess[CommonConstant.ROLE_CODE];
-      this.requestTaskModel.OfficeRoleCodes = [CommonConstant.SuperUser];
+      this.requestTaskModel.OfficeRoleCodes = [UserAccess[CommonConstant.ROLE_CODE]];
       
       this.IntegrationObj.baseUrl = URLConstant.GetAllTaskWorkflow;
       this.IntegrationObj.requestObj = this.requestTaskModel;
