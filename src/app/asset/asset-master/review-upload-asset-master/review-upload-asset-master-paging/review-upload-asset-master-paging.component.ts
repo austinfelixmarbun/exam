ed2e0f66c32dfc7e6799fd13de3c878645898fd2
calvin <calvin.cen@ad-ins.com>
2021-08-03 15:45:49 +0700

    Cekin Fixing Paging Office Role Codes UPL ASM

--- src/app/asset/asset-master/review-upload-asset-master/review-upload-asset-master-paging/review-upload-asset-master-paging.component.ts
+++ src/app/asset/asset-master/review-upload-asset-master/review-upload-asset-master-paging/review-upload-asset-master-paging.component.ts
@@ -40,7 +40,7 @@ export class ReviewUploadAssetMasterPagingComponent implements OnInit {
       this.requestTaskModel.OfficeCode = UserAccess[CommonConstant.OFFICE_CODE];
       this.requestTaskModel.TaskDefinitionKey = CommonConstant.WfUploadAssetMasterReview;
       this.requestTaskModel.RoleCode = UserAccess[CommonConstant.ROLE_CODE];
-      this.requestTaskModel.OfficeRoleCodes = [CommonConstant.SuperUser];
+      this.requestTaskModel.OfficeRoleCodes = [UserAccess[CommonConstant.ROLE_CODE]];
       
       this.IntegrationObj.baseUrl = URLConstant.GetAllTaskWorkflow;
       this.IntegrationObj.requestObj = this.requestTaskModel;
