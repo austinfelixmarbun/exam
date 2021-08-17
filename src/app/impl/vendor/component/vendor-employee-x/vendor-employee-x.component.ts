b1f73129aecfc953e06c9a58559a800b558f2f7d
Komang Anggada Sugiarta <komang.as@ad-ins.com>
2021-08-17 14:06:05 +0700

    fix regex email for vendor, office

--- src/app/impl/vendor/component/vendor-employee-x/vendor-employee-x.component.ts
+++ src/app/impl/vendor/component/vendor-employee-x/vendor-employee-x.component.ts
@@ -54,7 +54,7 @@ export class VendorEmployeeXComponent implements OnInit {
     BirthDate: [''],
     MobilePhnNo1: ['', [Validators.required, Validators.pattern("^[0-9]+$")]],
     MobilePhnNo2: ['', [Validators.pattern("^[0-9]+$")]],
-    Email: ['', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+    Email: ['', [Validators.pattern(CommonConstant.regexEmail)]],
     JoinDt: ['', [Validators.required]],
     VendorEmpRating: ['0'],
     Addr: [''],
--- src/app/impl/vendor/vendor-ho/vendor-ho-add-edit-x/vendor-ho-add-edit-x.component.ts
+++ src/app/impl/vendor/vendor-ho/vendor-ho-add-edit-x/vendor-ho-add-edit-x.component.ts
@@ -81,7 +81,7 @@ export class VendorHoAddEditXComponent implements OnInit {
     IdNo: [''],
     MobilePhnNo1: ['', Validators.pattern("^[0-9]+$")],
     MobilePhnNo2: ['', Validators.pattern("^[0-9]+$")],
-    Email: ['', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+    Email: ['', [Validators.pattern(CommonConstant.regexEmail)]],
     VendorRating: [''],
     EstablishmentDt: ['', Validators.required],
     PartnershipDt: ['', Validators.required],
