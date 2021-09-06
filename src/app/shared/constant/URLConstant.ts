28575edd52096bc4bf71ad053a2d1fa016c4d0d2
muhammad.sa <muhammad.sa@ad-ins.com>
2021-09-06 17:06:29 +0700

    SITDSFCFRTHREE-37: fix urlConstant DeleteCustPersonalFinData

--- src/app/shared/constant/URLConstant.ts
+++ src/app/shared/constant/URLConstant.ts
@@ -736,7 +736,7 @@ export class URLConstant {
     public static DeleteCustCompanyFinData = "/CustomerCompanyFinData/DeleteCustCompanyFinData";
     public static AddCustPersonalFinData = environment.FoundationR3Url + "/v1" + "/CustPersonalFinData/AddCustPersonalFinData";
     public static EditCustPersonalFinData = environment.FoundationR3Url + "/v1" + "/CustPersonalFinData/EditCustPersonalFinData";
-    public static DeleteCustPersonalFinData = "/CustPersonalFinData/DeleteCustPersonalFinData";
+    public static DeleteCustPersonalFinData = environment.FoundationR3Url + "/v1" + "/CustPersonalFinData/DeleteCustPersonalFinData";
     public static GetCustPersonalFinDataForCustViewByCustId = environment.FoundationR3Url + "/v1" + "/CustPersonalFinData/GetCustPersonalFinDataForCustViewByCustId";
     public static GetListCustPersonalFinDataForCustViewByCustId = environment.FoundationR3Url + "/v1" + "/CustPersonalFinData/GetListCustPersonalFinDataForCustViewByCustId";
     public static EditCustBankAcc = environment.FoundationR3Url + "/v1" + "/CustBankAcc/EditCustBankAcc";
