6ea86dc0e6acf983e472c21b68c64e8bddc6e55e
Marvin Sutanto <marvin.sutanto@ad-ins.com>
2021-09-14 08:35:55 +0700

    ubah dms url dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -10,7 +10,7 @@ export const environment = {
   //FoundationR3Url: 'http://localhost:5000',
   LosURL: 'http://r3app-server.ad-ins.com/LOS_DEV',
   FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
-  DMSUrl: "http://sky.ad-ins.com/LITEDMS_DSF/Integration/ViewDoc.aspx",
+  DMSUrl: "http://sky.ad-ins.com/LITEDMS_POC/Integration/ViewDoc.aspx",
   AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
   LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
   lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
@@ -22,7 +22,7 @@ export const environment = {
   WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
   WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
-  dmsURL: 'http://kfx-svr/LITEDMS_DSF/LiteDMS/pageconfins.aspx',
+  dmsURL: 'http://kfx-svr/LITEDMS_POC/LiteDMS/pageconfins.aspx',
   WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
   ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
   ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
