f2159bb8c9fafe08f3d65516715f102a78d235f7
muhammad.sa <muhammad.sa@ad-ins.com>
2021-10-07 10:47:02 +0700

    comment runtime configure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -98,15 +98,15 @@ const urlConstantConfig = (urlConfig: UrlConstantService) => {
         StorageService,
         NGXToastrService,
         ClaimTaskService,
-        UrlConstantNew,
-        EnviConfigService,
-        {
-            provide: APP_INITIALIZER, useFactory: appConfig, multi: true, deps: [EnviConfigService]
-        },
-        UrlConstantService,
-        {
-            provide: APP_INITIALIZER, useFactory: urlConstantConfig, multi: true, deps: [UrlConstantService]
-        },
+        // UrlConstantNew,
+        // EnviConfigService,
+        // {
+        //     provide: APP_INITIALIZER, useFactory: appConfig, multi: true, deps: [EnviConfigService]
+        // },
+        // UrlConstantService,
+        // {
+        //     provide: APP_INITIALIZER, useFactory: urlConstantConfig, multi: true, deps: [UrlConstantService]
+        // },
         { provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true }
     ],
     bootstrap: [AppComponent],
