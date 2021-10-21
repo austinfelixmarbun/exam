eab6f22239f50d56a26562d9e42d220716e9a9b8
Muhammad Syafiudin <muhammad.ss@ad-ins.com>
2021-10-21 12:07:18 +0700

    Issue Non Jira : Fix error jika ubah cust model via Main Data

--- src/app/customer/customer-personal/customer-personal-job-data/job-data-professional/job-data-professional.component.ts
+++ src/app/customer/customer-personal/customer-personal-job-data/job-data-professional/job-data-professional.component.ts
@@ -171,7 +171,7 @@ export class JobDataProfessionalComponent implements OnInit {
             PreviEmploymentDate: formatDate(this.returnCustJobDataObj.PrevEmploymentDt, 'yyyy-MM-dd', 'en-US'),
           });
 
-          if (!this.IsReset) {
+          if (!this.IsReset && this.returnCustJobDataObj.RefProfessionId) {
             this.http.post(URLConstant.GetRefProfessionById, { Id: this.returnCustJobDataObj.RefProfessionId }).subscribe(
               (response) => {
                 this.returnRefProfessionObj = response;
