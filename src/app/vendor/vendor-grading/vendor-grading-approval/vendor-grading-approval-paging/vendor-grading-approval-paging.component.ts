a1705e6fcb484005fd18ff5b19024a3b905f3563
calvin <calvin.cen@ad-ins.com>
2021-10-27 18:20:08 +0700

    Sweep Apv Get By OfficeCode

--- src/app/vendor/vendor-grading/vendor-grading-approval/vendor-grading-approval-paging/vendor-grading-approval-paging.component.ts
+++ src/app/vendor/vendor-grading/vendor-grading-approval/vendor-grading-approval-paging/vendor-grading-approval-paging.component.ts
@@ -47,6 +47,7 @@ export class VendorGradingApprovalPagingComponent implements OnInit {
       this.apvReqObj.CategoryCode = CommonConstant.VENDOR_GRADING_APV;
       this.apvReqObj.Username = this.userContext.UserName;
       this.apvReqObj.RoleCode = this.userContext.RoleCode;
+      this.apvReqObj.OfficeCode = this.userContext.OfficeCode;
       this.integrationObj.baseUrl = URLConstant.GetListOSApvTaskByCategoryCodeAndCurrentUserIdOrMainUserIdAndRoleCode;
       this.integrationObj.requestObj = this.apvReqObj;
       this.integrationObj.leftColumnToJoin = "VendorGradingHistNo";
