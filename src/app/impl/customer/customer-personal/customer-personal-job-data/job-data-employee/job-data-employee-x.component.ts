7c5245a7b892306327365a41e12640efb82dcbc2
Cynthia Purnomo <cynthia.purnomo@ad-ins.com>
2021-10-28 15:12:41 +0700

    SITDSFCFRTHREE-30 - field num of emp mandatory

--- src/app/impl/customer/customer-personal/customer-personal-job-data/job-data-employee/job-data-employee-x.component.ts
+++ src/app/impl/customer/customer-personal/customer-personal-job-data/job-data-employee/job-data-employee-x.component.ts
@@ -1,7 +1,7 @@
 import { formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 import { URLConstantX } from 'app/impl/shared/constant/URLConstantX';
@@ -111,7 +111,7 @@ export class JobDataEmployeeXComponent implements OnInit {
     PreviIndustryName: [''],
     PreviEmploymentDate: [''],
     NotesPreJob: [''],
-    NoOfEmploy: [''],
+    NoOfEmploy: [0, Validators.min(1)],
     CommodityCode: ['']
   });
   businessDtMin: Date;
