c29741a6319afe95e5fa7d0d09d300aec4e147c4
Andre Sucipto <andre.sucipto@ad-ins.com>
2021-11-04 15:34:16 +0700

    perbaikan vendor grading apv

--- src/app/vendor/vendor-grading/vendor-grading-approval/vendor-grading-approval-paging/vendor-grading-approval-paging.component.ts
+++ src/app/vendor/vendor-grading/vendor-grading-approval/vendor-grading-approval-paging/vendor-grading-approval-paging.component.ts
@@ -47,7 +47,8 @@ export class VendorGradingApprovalPagingComponent implements OnInit {
       this.apvReqObj.CategoryCode = CommonConstant.VENDOR_GRADING_APV;
       this.apvReqObj.Username = this.userContext.UserName;
       this.apvReqObj.RoleCode = this.userContext.RoleCode;
-      this.apvReqObj.OfficeCode = this.userContext.OfficeCode;
+      //agar bisa lintas cabang
+      //this.apvReqObj.OfficeCode = this.userContext.OfficeCode;
       this.integrationObj.baseUrl = URLConstant.GetListOSApvTaskByCategoryCodeAndCurrentUserIdOrMainUserIdAndRoleCode;
       this.integrationObj.requestObj = this.apvReqObj;
       this.integrationObj.leftColumnToJoin = "VendorGradingHistNo";
