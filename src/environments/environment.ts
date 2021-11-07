90b537849e6c9a6a172eabf07224ea696c42f7fa
Xania Yoan <xania.ya@ad-ins.com>
2021-11-07 12:50:30 +0700

    LOSRTHREE-975 : Fixed ApprovalR3Url URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,7 +15,7 @@ export const environment = {
   LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
   lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
   //ApprovalURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
-  ApprovalR3Url: 'http://r3app-server.ad-ins.com/Approval_R3_BE_SPRINGBOOT/',
+  ApprovalR3Url: 'http://r3app-server.ad-ins.com/Approval_R3_BE_SPRINGBOOT',
   ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_R3_SIT',
   FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
   losR3Web: 'http://r3web-server.ad-ins.com/LOSR3',
