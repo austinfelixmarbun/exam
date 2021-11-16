deaac88f90f6d0d6398d6e459d55256dc8bc0890
Muhammad Syafiudin <muhammad.ss@ad-ins.com>
2021-11-16 17:32:09 +0700

    Issue Non Jira: alih fungsi tombol "Sync Cust" ke "Submit to LMS"

--- src/app/impl/customer/customer-personal/customer-personal-page/customer-personal-page-x.component.ts
+++ src/app/impl/customer/customer-personal/customer-personal-page/customer-personal-page-x.component.ts
@@ -234,18 +234,4 @@ export class CustomerPersonalPageXComponent implements OnInit {
     }
     return flag;
   }
-
-  //START X-DSF INTEGRASI-CUST : 2021-10-29, Udin - Integrasi Cust FOU ke R2
-  submitToLmsX() {
-    this.http.post(URLConstant.GetCustByCustId, { Id: this.IdCust }).subscribe(
-      (response: CustObj) => {
-        this.http.post(URLConstantX.SendCustomerDataToRabbitMq, { CustNo: response.CustNo }).subscribe(
-          (response) => {
-            this.toastr.successMessage(response["Message"]);
-          }
-        )
-      }
-    );
-  }
-  //END X-DSF INTEGRASI-CUST
 }
