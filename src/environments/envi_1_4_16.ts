f69b4e9176da901458a12f9fb28456357c21c64e
Hendro Zhao <hendro.imp@ad-ins.com>
2021-11-29 17:16:05 +0700

    Setting environment 1.4.16

--- src/environments/envi_1_4_16.ts
+++ src/environments/envi_1_4_16.ts
@@ -4,29 +4,28 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-    production: true,
-    navbarColor: 'transparent',
-    //losUrl: 'http://localhost:5001',
-    //FoundationR3Url: 'http://localhost:5000',
-    losUrl: 'http://r3app-server.ad-ins.com/1.4.16/LOS',
-    FoundationR3Url: 'http://r3app-server.ad-ins.com/1.4.16/FOUNDATION',
-    DMSUrl: "http://sky.ad-ins.com/LITEDMS_POC/Integration/ViewDoc.aspx",
-    AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
-    LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
-    lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
-    //ApprovalURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
-    ApprovalR3Url: 'http://r3app-server.ad-ins.com/1.4.16/APPROVAL_R3_BE',
-    ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_R3_SIT',
-    FoundationR3Web: 'http://r3web-server.ad-ins.com/1.4.16/FOUNDATION',
-    losR3Web: 'http://r3web-server.ad-ins.com/1.4.16/LOS',
-    WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
-    WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
-    DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
-    dmsURL: 'http://kfx-svr/LITEDMS_POC/LiteDMS/pageconfins.aspx',
-    WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
-    ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
-    ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
-    Module: "FOU",
-    isCore: true
-  };
-  
+  production: true,
+  navbarColor: 'transparent',
+  //losUrl: 'http://localhost:5001',
+  //FoundationR3Url: 'http://localhost:5000',
+  losUrl: 'http://r3app-server.ad-ins.com/1.4.16/LOS',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/1.4.16/FOUNDATION',
+  DMSUrl: "http://sky.ad-ins.com/LITEDMS_POC/Integration/ViewDoc.aspx",
+  AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
+  LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
+  lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
+  //ApprovalURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
+  ApprovalR3Url: 'http://r3app-server.ad-ins.com/1.4.16/APPROVAL_R3_BE',
+  ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_R3_SIT',
+  FoundationR3Web: 'http://r3web-server.ad-ins.com/1.4.16/FOUNDATION',
+  losR3Web: 'http://r3web-server.ad-ins.com/1.4.16/LOS',
+  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
+  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
+  DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
+  dmsURL: 'http://kfx-svr/LITEDMS_POC/LiteDMS/pageconfins.aspx',
+  WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
+  ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
+  ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
+  Module: "FOU",
+  isCore: true
+};
