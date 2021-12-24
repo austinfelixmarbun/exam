208991f5228ed6916cfba14fad74955fd504a9be
Xania Yoan <xania.ya@ad-ins.com>
2021-12-24 13:51:40 +0700

    LOSRTHREE-1273 : Enhance Login & Change Role

--- src/app/shared/rolepick/rolepick.component.ts
+++ src/app/shared/rolepick/rolepick.component.ts
@@ -28,6 +28,27 @@ export class RolepickComponent implements OnInit, AfterViewInit {
   }
 
   chooseRole(item) {
+    var UserIdentityObj = {
+      RefUserId: item.RefUserId,
+      UserName: item.UserName,
+      EmpNo: item.EmpNo,
+      EmpName: item.EmpName,
+      OfficeId: item.RefOfficeId,
+      OfficeCode: item.OfficeCode,
+      OfficeName: item.OfficeName,
+      MrOfficeTypeCode: item.MrOfficeTypeCode,
+      RoleId: item.RefRoleId,
+      RoleCode: item.RoleCode,
+      RoleName: item.RoleName,
+      JobTitleId: item.RefJobTitleId,
+      JobTitleCode: item.JobTitleCode,
+      JobTitleName: item.JobTitleName,
+      BusinessDt: item.BusinessDt,
+      BusinessDtStr: item.BusinessDtStr,
+      Email: item.Email1,
+      CoyName: item.CoyName
+    }
+
     var roleObject = {
       UserName: this.data.user,
       Password: this.data.pwd,
@@ -36,11 +57,12 @@ export class RolepickComponent implements OnInit, AfterViewInit {
       JobTitleCode: item.JobTitleCode,
       RequestDateTime: item.BusinessDt,
       ModuleCode: environment.Module,
-      RowVersion: ""
-
+      RowVersion: "",
+      UserIdentityObj: UserIdentityObj
     };
+
     if (this.data.pwd == null) {
-      this.http.post(AdInsConstant.UpdateToken, roleObject, { withCredentials: true }).subscribe(
+      this.http.post(AdInsConstant.UpdateTokenV2, roleObject, { withCredentials: true }).subscribe(
         (response) => {
           //Cookie sudah diambil dari BE (Di set manual dulu)
 
@@ -67,10 +89,13 @@ export class RolepickComponent implements OnInit, AfterViewInit {
 
     }
     else {
-      this.http.post(AdInsConstant.LoginByRole, roleObject, { withCredentials: true }).subscribe(
+      this.http.post(AdInsConstant.LoginByRoleV2, roleObject, { withCredentials: true }).subscribe(
         (response) => {
           //Cookie sudah diambil dari BE (Di set manual dulu)
 
+          this.http.post(AdInsConstant.CheckUserSessionLog, roleObject, { withCredentials: true }).subscribe(
+            (response) => {});
+          
           var DateParse = formatDate(response["Identity"].BusinessDt, 'yyyy/MM/dd', 'en-US');
           AdInsHelper.SetCookie(this.cookieService, CommonConstant.TOKEN, response['Token']);
           AdInsHelper.SetCookie(this.cookieService, "XSRF-TOKEN", response['Token']);
--- src/app/shared/rolepick/rolepick.service.ts
+++ src/app/shared/rolepick/rolepick.service.ts
@@ -24,8 +24,7 @@ export class RolePickService {
                 RowVersion: ""
             };
 
-            let LoginByTokenURL = environment.isCore ? AdInsConstant.LoginByTokenV2 : AdInsConstant.LoginByToken;
-            this.http.post(LoginByTokenURL, roleObject2).subscribe(
+            this.http.post(AdInsConstant.LoginByTokenV2, roleObject2).subscribe(
                 (response) => {
                     const object = {
                         response: response[CommonConstant.ReturnObj]
@@ -49,6 +48,27 @@ export class RolePickService {
         } else {
             if (data.response.length == 1 && type == "") {
                 var item = data.response[0];
+                var UserIdentityObj = {
+                    RefUserId: item.RefUserId,
+                    UserName: item.UserName,
+                    EmpNo: item.EmpNo,
+                    EmpName: item.EmpName,
+                    OfficeId: item.RefOfficeId,
+                    OfficeCode: item.OfficeCode,
+                    OfficeName: item.OfficeName,
+                    MrOfficeTypeCode: item.MrOfficeTypeCode,
+                    RoleId: item.RefRoleId,
+                    RoleCode: item.RoleCode,
+                    RoleName: item.RoleName,
+                    JobTitleId: item.RefJobTitleId,
+                    JobTitleCode: item.JobTitleCode,
+                    JobTitleName: item.JobTitleName,
+                    BusinessDt: item.BusinessDt,
+                    BusinessDtStr: item.BusinessDtStr,
+                    Email: item.Email1,
+                    CoyName: item.CoyName
+                  }
+
                 var roleObject = {
                     UserName: data.user,
                     Password: data.pwd,
@@ -58,10 +78,10 @@ export class RolePickService {
                     RequestDateTime: item.BusinessDt,
                     RowVersion: "",
                     Ip: "",
-                    ModuleCode: environment.Module
-
+                    ModuleCode: environment.Module,
+                    UserIdentityObj: UserIdentityObj
                 };
-                this.http.post(AdInsConstant.LoginByRole, roleObject, { withCredentials: true }).subscribe(
+                this.http.post(AdInsConstant.LoginByRoleV2, roleObject, { withCredentials: true }).subscribe(
                     (response) => {
                         //Cookie sudah diambil dari BE (Di set manual dulu)
 
