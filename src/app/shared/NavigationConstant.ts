5c0ef2cec1f5255f3c164e72266594f17d200f5c
Stephen Hermanto <stephen.hermanto@ad-ins.com>
2021-12-27 14:32:57 +0700

    Revert "Merge branch 'FOU_DSF_IMPL' of https://tfs.ad-ins.com/tfs/CONFINSR3/_git/FoundationR3 into RaxellDsfImplGit"
    This reverts commit fafb91b412b3ae0ded68deca8b75d9f5791e7fcd, reversing
    changes made to ca0b882eb90e0f8c1f6b2f964eb03e6ce919e924.

--- src/app/shared/NavigationConstant.ts
+++ src/app/shared/NavigationConstant.ts
@@ -150,7 +150,7 @@ export class NavigationConstant {
     public static VIEW_CUST_COY_DETAIL = NavigationConstant.VIEW_CUST + "/" + PathConstant.VIEW_CUST_COY_DETAIL; //'/View/Customer/CoyDetail'
     public static VIEW_CUST_COY_MNGMNT = NavigationConstant.VIEW_CUST + "/" + PathConstantX.VIEW_CUST_COY_MNGMNT_X; //'/View/Customer/CoyManagement/X'
     public static VIEW_CUST_COY_CONTACT = NavigationConstant.VIEW_CUST + "/" + PathConstant.VIEW_CUST_COY_CONTACT; //'/View/Customer/CoyContact'
-    public static VIEW_CUST_COY_FINANCIAL = NavigationConstant.VIEW_CUST + "/" + PathConstantX.VIEW_CUST_COY_FINANCIAL_X; //'/View/Customer/CoyFinancial'
+    public static VIEW_CUST_COY_FINANCIAL = NavigationConstant.VIEW_CUST + "/" + PathConstant.VIEW_CUST_COY_FINANCIAL; //'/View/Customer/CoyFinancial'
     public static VIEW_CUST_COY_LEGAL = NavigationConstant.VIEW_CUST + "/" + PathConstant.VIEW_CUST_COY_LEGAL; //'/View/Customer/CoyLegal'
     public static VIEW_CUST_TRUSTING_SOCIAL = NavigationConstant.VIEW_CUST + "/" + PathConstant.VIEW_CUST_TRUSTING_SOCIAL; //'/View/Customer/TrustSoc'
     public static VIEW_NEG_CUST = PathConstant.CR_VIEW + "/" + PathConstant.VIEW_NEG_CUST; //'/View/NegativeCustomer'
--- src/app/view/customer-view/customer-view-coy-financial/customer-view-coy-financial.module.ts
+++ src/app/view/customer-view/customer-view-coy-financial/customer-view-coy-financial.module.ts
@@ -17,12 +17,6 @@ import { CustomerViewCoyFinancialComponent } from "./customer-view-coy-financial
 import { CustomerViewCoyFinancialRoutingModule } from "./customer-view-coy-financial-routing.module";
 import { CustomerViewCoyFinancialSectionComponent } from "../customer-view-coy-financial-section/customer-view-coy-financial-section.component";
 import { AdInsSharedModule } from "app/components/adins-module/adins-shared.module";
-import {
-  CustomerViewCoyFinancialSectionXComponent
-} from '../../../impl/view/customer-view/customer-view-coy-financial-section/customer-view-coy-financial-section-x.component';
-import {
-  CustomerViewCoyFinancialXComponent
-} from '../../../impl/view/customer-view/customer-view-coy-financial/customer-view-coy-financial-x.component';
 
 
 
@@ -48,9 +42,7 @@ import {
   ],
   declarations: [
     CustomerViewCoyFinancialComponent,
-    CustomerViewCoyFinancialSectionComponent,
-    CustomerViewCoyFinancialXComponent,
-    CustomerViewCoyFinancialSectionXComponent
+    CustomerViewCoyFinancialSectionComponent
   ],
   entryComponents: [
     UcviewgenericComponent,
