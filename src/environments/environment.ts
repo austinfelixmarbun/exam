0c54e66439ea62c4f680db5c738ce59a689509de
ReynardW <reynard.wf@ad-ins.com>
2021-12-27 14:18:03 +0700

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// testing comment
 export const environment = {
   production: false,
   navbarColor: 'transparent',
