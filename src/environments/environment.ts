c2d655c7defb6342d5f7ae256d8b9e0c1fa809a6
shafiq.affandi <muhammad.sa@ad-ins.com>
2022-02-10 16:03:24 +0700

    tambah module cms pada select module

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,6 +19,7 @@ export const environment = {
   ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_R3_SIT',
   FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
   losR3Web: 'http://r3web-server.ad-ins.com/LOSR3',
+  cmsR3Web: 'http://r3web-server.ad-ins.com/CMS',
   WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
   WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
