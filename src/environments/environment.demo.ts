c6e1bbbb45245a45a6cecca2eb152fe73116083f
Darren <darren@ad-ins.com>
2022-02-23 15:25:24 +0700

    envi buat CMS demo BSI

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -5,16 +5,28 @@
 
 export const environment = {
   production: true,
-  navbarColor: 'lightgray',
-  LosURL: 'http://r3web.ad-ins.com/LOS_DEMO/',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEMO',
-  WebSocketURL : 'http://r3app-server.ad-ins.com/FOUNDATION_DEMO',
-  losR3Web: 'http://r3web-server.ad-ins.com/LOS_DEMO',
-  lmsWeb: 'http://r3impl-websvr.ad-ins.com/LMS',
-  Module:"FOU",
-  ApprovalURL : 'http://r3app-server.ad-ins.com/APPROVAL_DEMO',
-  FoundationR3Web:'http://r3web.ad-ins.com/FOUNDATION_DEMO',
+  navbarColor: 'transparent',
+  //losUrl: 'http://localhost:5001',
+  //FoundationR3Url: 'http://localhost:5000',
+  LosURL: 'http://r3app-server.ad-ins.com/LOS_DEV',
+  FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/DEMO_BSI/FOUNDATION_DEV_DEMO_BSI',
+  DMSUrl: "http://sky.ad-ins.com/LITEDMS_POC/Integration/ViewDoc.aspx",
+  AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
+  LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
+  lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
+  //ApprovalURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
+  ApprovalR3Url: 'http://r3app-server.ad-ins.com/Approval_R3_BE_SPRINGBOOT',
+  ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_R3_SIT',
+  FoundationR3Web: 'http://r3impl-websvr.ad-ins.com/DEMO_BSI/FOUNDATION_DEMO_BSI',
+  losR3Web: 'http://r3web-server.ad-ins.com/LOS_DEV',
+  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
+  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
+  DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
+  dmsURL: 'http://kfx-svr/LITEDMS_POC/LiteDMS/pageconfins.aspx',
+  WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
   ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
   ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
-  DMSUrl: "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
+  Module: "FOU",
+  isCore: true,
+  SpinnerOnHttpPost: false
 };
