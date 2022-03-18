9d57d40e6b8d61dab07202b67e2857b38dadaba3
Edward Bala Yansen <edward.by@ad-ins.com>
2022-03-18 10:00:48 +0700

    pathConst kurang

--- src/app/shared/PathConstant.ts
+++ src/app/shared/PathConstant.ts
@@ -227,11 +227,14 @@ export class PathConstant {
     public static CUST_UPDATE_DATA_PAGING = PathConstant.CUST_UPDATE_DATA + "/" + PathConstant.PAGING;
     public static CUST_UPDATE_DATA_DETAIL = PathConstant.CUST_UPDATE_DATA + "/" + PathConstant.DETAIL;
     public static CUST_NEG = "NegativeCustomer";
+    public static CUST_NEG_VIEW = PathConstant.CUST_NEG + "/" + PathConstant.VIEW;
     public static CUST_NEG_PAGING = PathConstant.CUST_NEG + "/" + PathConstant.PAGING;
     public static CUST_NEG_DETAIL = PathConstant.CUST_NEG + "/" + PathConstant.DETAIL;
     public static CUST_NEG_UPLOAD = PathConstant.CUST_NEG + "/" + PathConstant.UPLOAD;
     public static CUST_NEG_RVW_UPLOAD_PAGING = PathConstant.CUST_NEG + "/" + PathConstant.RVW_UPLOAD_PAGING;
     public static CUST_NEG_RVW_UPLOAD_DETAIL = PathConstant.CUST_NEG + "/" + PathConstant.RVW_UPLOAD_DETAIL;
+    public static CUST_VIEW = "CustomerView";
+    public static CUST_VIEW_PAGE = PathConstant.CUST_VIEW + "/" + PathConstant.CUST_PAGE;
     public static NEW_CUST = "NewCustomer";
     //#endregion
     
