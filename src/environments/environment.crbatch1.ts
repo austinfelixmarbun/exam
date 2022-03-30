113f8ef29c8edf19d96a36e4da2a2232bfd3a825
richard suprayogi <richard.suprayogi@ad-ins.com>
2022-03-31 01:23:04 +0700

    penambahan envi.ts untuk CR BATCH 1

---
+++ src/environments/environment.crbatch1.ts
@@ -0,0 +1,35 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  navbarColor: 'transparent',
+  //losUrl: 'http://localhost:5001',
+  //FoundationR3Url: 'http://localhost:5000',
+//   LosURL: 'http://r3impl-appsvr.ad-ins.com/LOS_DSF_IMPL',
+//   FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUNDATION_DSF_IMPL',
+  LosURL: 'http://r3impl-appsvr.ad-ins.com/LOS_DSF_IMPL_CR_BATCH_1',
+  FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUNDATION_DSF_IMPL_CR_BATCH_1',
+  DMSUrl: "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
+  AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
+  LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
+  lmsWeb: "http://r3impl-websvr.ad-ins.com/LMS", //sementara ku tak tau
+  //ApprovalURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
+  ApprovalURL: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_R3_BE_DSF_IMPL_CAMUNDA',
+  ApprovalR3Url: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_R3_BE_DSF_IMPL_CAMUNDA',
+  FoundationR3Web: 'http://r3impl-websvr.ad-ins.com/FOU_DSF_IMPL',
+  // FoundationR3Url: 'http://localhost:5000',
+  losR3Web: 'http://r3impl-websvr.ad-ins.com/LOS_DSF_IMPL',
+  WorkflowR3Url: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_DSF_IMPL',
+  WebSocketURL: 'http://r3impl-appsvr.ad-ins.com/FOU_DSF_IMPL',
+  DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
+  dmsURL: 'http://kfx-svr/LITEDMS_POC/LiteDMS/pageconfins.aspx',
+  WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
+  ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
+  ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
+  Module: "FOU",
+  isCore: true,
+  SpinnerOnHttpPost: false
+};
