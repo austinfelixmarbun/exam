b0dbb08e3390892f816c578cc22a53ffa90097f0
ryan.rg <ryan.rg@ad-ins.com>
2022-04-04 11:59:24 +0700

    Sweeping Refresh Parameter Foundation

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -79,8 +79,8 @@ export class SidebarComponent implements OnInit {
     navigateSkipLocationChange(ev) {
         //sementara Sementara begini dulu, belum ketemu solusi lain
         //problem : ketika di 'click' halaman memasuki halaman /dashboard/dash-empty terlebih dahulu
-        this.router.navigateByUrl(NavigationConstant.DASHEMPTY, { skipLocationChange: true }).then(() => {
-            AdInsHelper.RedirectUrl(this.router, [ev.Path], this.genParam(ev.Params), false);
-        });
+        AdInsHelper.RedirectUrl(this.router, [ev.Path], this.genParam(ev.Params), false);
+        // this.router.navigateByUrl(NavigationConstant.DASHEMPTY, { skipLocationChange: true }).then(() => {
+        // });
     }
 }
