e1f09e3e0fa89dfb10359726f3535987e529116f
agnes.ga <agnes.ga@ad-ins.com>
2022-05-27 15:53:16 +0700

    SITUFCFRTHREE-19 : disable edit legal doc type

--- src/app/customer/cust-legal-doc/cust-legal-doc-detail/cust-legal-doc-detail.component.ts
+++ src/app/customer/cust-legal-doc/cust-legal-doc-detail/cust-legal-doc-detail.component.ts
@@ -71,6 +71,7 @@ export class CustLegalDocDetailComponent implements OnInit {
     );
 
     if(this.Mode == "Edit"){
+      this.CustCompanyLegalDocForm.controls.MrLegalDocTypeCode.disable();
       this.httpClient.post(URLConstant.GetCustCompanyLegalDocByCustCompanyLegalDocId, {Id : this.CustCompanyLegalDocId}).subscribe(
         (response: CustCompanyLegalDocObj) => {
           this.CustCompanyLegalDocForm.patchValue({
