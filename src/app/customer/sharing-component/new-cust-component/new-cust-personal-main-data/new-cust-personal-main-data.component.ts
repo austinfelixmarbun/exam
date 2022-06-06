41e5bd62cc6a98e243e9c6fe57a6d4886a69ef12
ryan.rg <ryan.rg@ad-ins.com>
2022-06-06 16:11:44 +0700

    Fix Validasi SITUFCFRTHREE-82 & SITUFCFRTHREE-83

--- src/app/customer/sharing-component/new-cust-component/new-cust-personal-main-data/new-cust-personal-main-data.component.ts
+++ src/app/customer/sharing-component/new-cust-component/new-cust-personal-main-data/new-cust-personal-main-data.component.ts
@@ -229,7 +229,8 @@ export class NewCustPersonalMainDataComponent implements OnInit {
       MrCustRelationship: [''],
       MrCustModelCode: ['', [Validators.required]],
       MobilePhnNo1: ['', [Validators.required, Validators.pattern("^[0-9]+$")]],
-      Email1: ['', Validators.pattern(CommonConstant.regexEmail)]
+      Email1: ['', [Validators.required, Validators.pattern(CommonConstant.regexEmail)]]
+      
     });
 
     if (this.CustDataMode != this.CustDataModeMain) {
@@ -244,6 +245,8 @@ export class NewCustPersonalMainDataComponent implements OnInit {
       this.CustomerForm.get("MrCustModelCode").updateValueAndValidity();
     }
     if (this.CustDataMode == this.CustDataModeFamily) {
+      this.CustomerForm.get("Email1").setValidators(Validators.pattern(CommonConstant.regexEmail));
+      this.CustomerForm.get("Email1").updateValueAndValidity();
       this.CustomerForm.get("MrCustRelationship").setValidators(Validators.required);
       this.CustomerForm.get("MrCustRelationship").updateValueAndValidity();
     }
