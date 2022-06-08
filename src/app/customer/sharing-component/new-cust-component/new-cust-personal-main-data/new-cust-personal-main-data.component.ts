6660306c013d45a2eea5155b9c5917416cf2410e
ryan.rg <ryan.rg@ad-ins.com>
2022-06-08 14:11:29 +0700

    Reopen SIT 83 Angular 7

--- src/app/customer/sharing-component/new-cust-component/new-cust-personal-main-data/new-cust-personal-main-data.component.ts
+++ src/app/customer/sharing-component/new-cust-component/new-cust-personal-main-data/new-cust-personal-main-data.component.ts
@@ -243,11 +243,13 @@ export class NewCustPersonalMainDataComponent implements OnInit {
       this.CustomerForm.get("MrCustModelCode").clearValidators();
       this.CustomerForm.get("MrCustModelCode").updateValueAndValidity();
     }
-    if (this.CustDataMode == this.CustDataModeFamily || this.pageFrom == CommonConstant.CustFromCustFamily) {
-      this.CustomerForm.get("Email1").setValidators(Validators.pattern(CommonConstant.regexEmail));
-      this.CustomerForm.get("Email1").updateValueAndValidity();
+    if (this.CustDataMode == this.CustDataModeFamily) {
       this.CustomerForm.get("MrCustRelationship").setValidators(Validators.required);
       this.CustomerForm.get("MrCustRelationship").updateValueAndValidity();
+    }
+    if(this.pageFrom == CommonConstant.CustFromCustFamily){
+      this.CustomerForm.get("Email1").setValidators(Validators.pattern(CommonConstant.regexEmail));
+      this.CustomerForm.get("Email1").updateValueAndValidity();
     }
   }
   //#endregion
