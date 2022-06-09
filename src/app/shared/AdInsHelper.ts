3841bfcfa00fefc1fc0f48e5cc4a73ba7a034c3e
shafiq.affandi <muhammad.sa@ad-ins.com>
2022-06-09 13:57:27 +0700

    add mekanisme refresh redirect jika tidak ada param

--- src/app/shared/AdInsHelper.ts
+++ src/app/shared/AdInsHelper.ts
@@ -172,15 +172,22 @@ export class AdInsHelper {
 
     public static RedirectUrl(router: Router, url: Array<string>, queryParams: {} = {}, isSkipLocation: boolean = false) {
         // Ngebuat bisa jalanin Constructor dan NgOnInit lagi
-        // router.routeReuseStrategy.shouldReuseRoute = () => {
-        //     return false;
-        // }
+        const prev = router.routeReuseStrategy.shouldReuseRoute;
+        if (Object.keys(queryParams).length === 0 
+            && queryParams.constructor === Object) {
+            router.routeReuseStrategy.shouldReuseRoute = () => {
+                return false;
+            }
+        }
         // router.navigateByUrl(
         //   router.createUrlTree(
         //     [url.toString()], { queryParams: queryParams }
         //   ), { skipLocationChange: isSkipLocation }
         // );
         router.navigate(url, { queryParams: queryParams, skipLocationChange: isSkipLocation });
+        setTimeout(() => {
+            router.routeReuseStrategy.shouldReuseRoute = prev;
+        }, 1);
     }
 
     public static RedirectUrlView(router: Router, url: Array<string>, queryParams: {}, isSkipLocation: boolean = false) {
