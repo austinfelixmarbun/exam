b8d5f3c8bd174d46563e8fb7148d8a6bb9da8780
Muhammad Syafiudin <muhammad.ss@ad-ins.com>
2022-06-27 11:29:43 +0700

    Fix Merging

--- src/app/vendor/component/vendor/vendor-paging/vendor-paging.component.ts
+++ src/app/vendor/component/vendor/vendor-paging/vendor-paging.component.ts
@@ -33,22 +33,6 @@ export class VendorPagingComponent implements OnInit, OnDestroy {
     }
   }
 
-  ReInit(){
-    this.IsReady = false;
-    this.inputPagingObj = new UcPagingObj();
-    this.Type = "Default";
-  }
-
-  RefetchData(){
-    this.ReInit();
-    this.SubscribeParam();
-    this.SelectPage();
-    setTimeout (() => {
-      this.IsReady = true;
-    }, 10);
-  }
-
-  
   ngOnInit() {
     this.selectPage();
   }
