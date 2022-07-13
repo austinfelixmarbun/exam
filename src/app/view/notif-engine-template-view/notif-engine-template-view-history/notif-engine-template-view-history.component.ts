9922cffa4025e019365af6838663ea95fdd8463f
ryan.rg <ryan.rg@ad-ins.com>
2022-07-13 16:50:56 +0700

    update ketinggalan variable gaguna

--- src/app/view/notif-engine-template-view/notif-engine-template-view-history/notif-engine-template-view-history.component.ts
+++ src/app/view/notif-engine-template-view/notif-engine-template-view-history/notif-engine-template-view-history.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NotificationTemplateObj } from 'app/shared/model/notif-engine/notification-template-obj.model';
 import { UrlConstantNew } from 'app/shared/constant/URLConstantNew';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
 import { InputGridObj } from 'app/shared/model/input-grid-obj.model';
 
 @Component({
@@ -10,37 +9,28 @@ import { InputGridObj } from 'app/shared/model/input-grid-obj.model';
   templateUrl: './notif-engine-template-view-history.component.html',
 })
 export class NotifEngineTemplateViewHistoryComponent implements OnInit {
-  listTemplateHist: Array<NotificationTemplateObj> = new Array<NotificationTemplateObj>();
-  TemplateCode: string = "";
-  TemplateVersion: number = 0;
+  
+  @Input() TemplateCode: string;
   InputGridTemplateHistObj: InputGridObj = new InputGridObj();
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private UrlConstantNew: UrlConstantNew) {
-    this.route.queryParams.subscribe(params => {
-      if (params["TemplateCode"] != null) {
-        this.TemplateCode = params["TemplateCode"];
-      }
-      if (params["TemplateVersion"] != null) {
-        this.TemplateVersion = params["TemplateVersion"];
-      }
-    });
-   }
+  constructor(private http: HttpClient, private UrlConstantNew: UrlConstantNew) { }
 
   async ngOnInit() {
     this.InputGridTemplateHistObj.pagingJson = "./assets/ucgridview/notif-engine/grid-notif-template-history-view.json";
-    await this.GetListTemplateHist();
+    let listTemplateHist: Array<NotificationTemplateObj> = new Array<NotificationTemplateObj>();
     this.InputGridTemplateHistObj.resultData = {
       Data: ""
     }
     this.InputGridTemplateHistObj.resultData["Data"] = new Array();
-    this.InputGridTemplateHistObj.resultData.Data = this.listTemplateHist;
+    this.InputGridTemplateHistObj.resultData.Data = await this.GetListTemplateHist(listTemplateHist);
   }
 
-  async GetListTemplateHist() {
+  async GetListTemplateHist(listTemplateHist: Array<NotificationTemplateObj>) {
     await this.http.post(this.UrlConstantNew.GetListNotificationTemplateByNotificationTemplateCode, { Code: this.TemplateCode }).toPromise().then(
       (response: Array<NotificationTemplateObj>) => {
-        this.listTemplateHist = response;
+        listTemplateHist = response;
       }
     );
+    return listTemplateHist;
   }
 }
