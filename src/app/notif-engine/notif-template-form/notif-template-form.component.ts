4893c89055f18cfbe563a97655a43487c4f67ff4
Edward Bala Yansen <edward.by@ad-ins.com>
2022-07-28 15:12:52 +0700

    R3TECH-138 - notif template CheckValidatorEndDt

--- src/app/notif-engine/notif-template-form/notif-template-form.component.ts
+++ src/app/notif-engine/notif-template-form/notif-template-form.component.ts
@@ -12,6 +12,8 @@ import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 import { CommonConstant } from 'app/shared/constant/CommonConstant';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { RefNotifAttrTemplateObj } from 'app/shared/model/notif-engine/ref-notif-attr-template-obj.model';
+import { DatePipe } from '@angular/common';
+import { ExceptionConstant } from 'app/shared/constant/ExceptionConstant';
 
 @Component({
   selector: 'app-notif-template-form',
@@ -106,8 +108,8 @@ export class NotifTemplateFormComponent implements OnInit {
           MrNotificationLevelCode: response.MrNotificationLevelCode,
           MrNotificationSourceCode: response.MrNotificationSourceCode,
           MrNotificationTypeCode: response.MrNotificationTypeCode,
-          StartDt: response.StartDt,
-          EndDt: response.EndDt,
+          StartDt: this.toDateString(response.StartDt),
+          EndDt: this.toDateString(response.EndDt),
           Subject: response.Subject,
           Body: response.Body,
           BaseUrl: response.BaseUrl,
@@ -119,6 +121,14 @@ export class NotifTemplateFormComponent implements OnInit {
         }
       }
     )
+  }
+
+  private toDateString(dt: Date): string {
+    let date = new Date(dt);
+    return (date.getFullYear().toString() + '-'
+      + ("0" + (date.getMonth() + 1)).slice(-2) + '-'
+      + ("0" + (date.getDate())).slice(-2))
+      + 'T' + date.toTimeString().slice(0, 5);
   }
 
   GetListActiveRefNotifAttrTemplate() {
@@ -208,7 +218,16 @@ export class NotifTemplateFormComponent implements OnInit {
     this.TempMessage.InputParamValue();
   }
 
+  private CheckValidatorEndDt() {
+    let datePipe = new DatePipe("en-US");
+    let startDt = this.GetStartDt;
+    let endDt = this.NotifTemplateForm.get("EndDt").value;
+    if (startDt > endDt) {
+      throw this.toastr.warningMessage(ExceptionConstant.END_DATE_MUST_EQUAL_OR_MORE_THAN + " " + datePipe.transform(startDt, 'MMMM d, y'));
+    }
+  }
   async SaveForm() {
+    this.CheckValidatorEndDt();
     let urlSave: string = this.UrlConstantNew.AddNotificationTemplate;
     if (this.NotificationTemplateSaveObj.NotificationTemplateId != 0) urlSave = this.UrlConstantNew.EditNotificationTemplate;
     await this.http.post(urlSave, this.SetSaveObj(), AdInsConstant.SpinnerOptions).toPromise().then(
