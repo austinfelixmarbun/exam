e3499a7e86de99aac20d506476f553d3c6a7310b
teresa.octaviani <teresa.octaviani@ad-ins.com>
2022-11-17 10:11:53 +0700

    R3LOS-161 : RTHREE-407 - Fixing View ASLI RI if not ready

--- src/app/view/customer-view/customer-view-asli-ri/customer-view-asli-ri.component.ts
+++ src/app/view/customer-view/customer-view-asli-ri/customer-view-asli-ri.component.ts
@@ -41,7 +41,7 @@ export class CustomerViewAsliRiComponent implements OnInit {
   MrCustModelName: string;
   IDType: string;
   DataAsliRi: any;
-  isReady: boolean;
+  isReady: boolean = false;
   img: any;
   url: string;
 
