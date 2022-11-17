a9b53ac4a7cbbc7e8c70b800b949dac4ee2878ca
teresa.octaviani <teresa.octaviani@ad-ins.com>
2022-11-17 09:25:39 +0700

    no message

--- src/app/customer/customer-personal/customer-personal-job-data/job-data-small-medium-enterprise/job-data-small-medium-enterprise.component.ts
+++ src/app/customer/customer-personal/customer-personal-job-data/job-data-small-medium-enterprise/job-data-small-medium-enterprise.component.ts
@@ -165,6 +165,11 @@ export class JobDataSmeComponent implements OnInit {
         this.listCustAddr = this.listCustAddr.filter(x => x.MrCustAddrTypeCode != CommonConstant.CustAddrJob && 
                                                           x.MrCustAddrTypeCode != CommonConstant.CustAddrPreJob && 
                                                           x.MrCustAddrTypeCode != CommonConstant.CustAddrOthBiz);
+        this.JobDataSmeForm.patchValue({
+          CopyAddrFrom: response[CommonConstant.ReturnObj][0]['CustAddrId'],
+          CopyPrevAddrFrom: response[CommonConstant.ReturnObj][0]['CustAddrId'],
+          CopyOthBizAddrFrom: response[CommonConstant.ReturnObj][0]['CustAddrId']
+        });
       });
 
     this.inputAddressObjForJobAddr = new InputAddressObj();
