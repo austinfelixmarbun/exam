bce76a609678a65c698bf467c398b9958c41ff1c
agnes.ga <agnes.ga@ad-ins.com>
2022-11-21 08:27:31 +0700

    R3LOS-153 : RTHREE-403 - Issue Vendor Grading Request
    (cherry picked from commit 8f6ca3e47638474949ab84092fbd7c84b23a3d42)

--- src/app/dashboard/dash-board/dash-board.component.ts
+++ src/app/dashboard/dash-board/dash-board.component.ts
@@ -55,13 +55,23 @@ export class DashBoardComponent implements OnInit {
       integrationObj.RequestObj.UserName = this.username;
       integrationObj.RequestObj.OfficeRoleCodes = [this.roleCode, this.roleCode + "-" + this.officeCode, this.officeCode];
       
-      integrationObj2 = new ThingsToDoIntegrationV2Obj();
-      integrationObj2.BaseUrl = AdInsConstant.GetListApvTaskListByUsernameAndRoleCodeForThingsToDo;
-      integrationObj2.ApiPath = "";
-      integrationObj2.RequestObj.OfficeCode = this.officeCode;
-      integrationObj2.RequestObj.UserName = this.username;
-      integrationObj2.RequestObj.RoleCode = this.roleCode;
-      this.Item.RequestObj.IntegrationObj.push(integrationObj2);
+      if (this.officeCode == CommonConstant.HeadOffice) {
+        integrationObj2 = new ThingsToDoIntegrationV2Obj();
+        integrationObj2.BaseUrl = AdInsConstant.GetListApvTaskListByUsernameAndRoleCodeForThingsToDo;
+        integrationObj2.ApiPath = "";
+        integrationObj2.RequestObj.OfficeCode = "";
+        integrationObj2.RequestObj.UserName = this.username;
+        integrationObj2.RequestObj.RoleCode = this.roleCode;
+        this.Item.RequestObj.IntegrationObj.push(integrationObj2);
+      } else {
+        integrationObj2 = new ThingsToDoIntegrationV2Obj();
+        integrationObj2.BaseUrl = AdInsConstant.GetListApvTaskListByUsernameAndRoleCodeForThingsToDo;
+        integrationObj2.ApiPath = "";
+        integrationObj2.RequestObj.OfficeCode = this.officeCode;
+        integrationObj2.RequestObj.UserName = this.username;
+        integrationObj2.RequestObj.RoleCode = this.roleCode;
+        this.Item.RequestObj.IntegrationObj.push(integrationObj2);
+      }
     }else{
       integrationObj = new ThingsToDoIntegrationObj();
       integrationObj.RequestObj.Office = "";
