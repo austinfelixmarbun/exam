591eef18bb5bd40b33643d542fa130cf75b0afca
Willen <willen.febryanto@ad-ins.com>
2023-05-12 19:00:41 +0700

    Fix:Ubah_Penempatan_Route_CashierView

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -359,18 +359,7 @@ export const Full_ROUTES: Routes = [
         .then(m => m.CashierTransactionModule)
         .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
     }
-  },
-  {
-    path: 'cashierview',
-    loadChildren: () => {
-      return loadRemoteModule({
-        type: 'module',
-        remoteEntry: envi.cashbankR3Web + '/remoteEntry.js',
-        exposedModule: './CashierTransactionModule'
-      })
-        .then(m => m.CashierTransactionModule)
-        .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
-    }
   }
+  
   //#endregion
 ];
