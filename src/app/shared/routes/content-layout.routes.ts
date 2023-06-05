56fba1875ffcca5fafb561f3dd30d00be272dfa4
Willen <willen.febryanto@ad-ins.com>
2023-06-05 18:06:18 +0700

    Fix:Payment_Receive_View

--- src/app/shared/routes/content-layout.routes.ts
+++ src/app/shared/routes/content-layout.routes.ts
@@ -73,9 +73,9 @@ export const CONTENT_ROUTES: Routes = [
       return loadRemoteModule({
         type: 'module',
         remoteEntry: envi.cashbankR3Web + '/remoteEntry.js',
-        exposedModule: './PaymentReceive'
+        exposedModule: './'
       })
-        .then(m => m.PaymentReceiveModule)
+        .then(m => m.PrepaidAllocModule)
         .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
     }
   },
