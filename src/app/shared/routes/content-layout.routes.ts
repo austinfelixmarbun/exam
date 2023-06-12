0d4a5e2faff0f1eaaffeb4270c535448e075d03a
Venicia <venicia.setiani@ad-ins.com>
2023-06-12 18:12:50 +0700

    pdc cancel apv view

--- src/app/shared/routes/content-layout.routes.ts
+++ src/app/shared/routes/content-layout.routes.ts
@@ -322,5 +322,18 @@ export const CONTENT_ROUTES: Routes = [
             .then(m => m.ChargeReceivableModule)
             .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
         }
-      }
+      },
+      {
+        path: 'pdccancelview',
+        loadChildren: () => {
+          return loadRemoteModule({
+            type: 'module',
+            remoteEntry: envi.pdcR3Web + '/remoteEntry.js',
+            exposedModule: './PdcCancelModule'
+          })
+            .then(m => m.PdcCancelModule)
+            .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
+        }
+      },
+
 ];
