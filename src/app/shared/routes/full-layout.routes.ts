b433a6cab1f3f10a4d22f71a187674dd2a48e01f
Willen <willen.febryanto@ad-ins.com>
2023-06-16 14:39:14 +0700

    Fix:Payment_Channel_Receive_Inquiry

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -253,6 +253,18 @@ export const Full_ROUTES: Routes = [
     }
   },
   {
+    path: 'payment-channel-receive',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'module',
+        remoteEntry: envi.paymentR3Web + '/remoteEntry.js',
+        exposedModule: './PaymentChannel'
+      })
+        .then(m => m.PaymentChannelModule)
+        .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
+    }
+  },
+  {
     path: 'payment',
     loadChildren: () => {
       return loadRemoteModule({
