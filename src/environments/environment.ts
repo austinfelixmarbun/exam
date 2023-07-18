6b7f4006e00f06bccf9b31d7c41cef89f8998e7f
Yonita Suryani <yonita.suryani@ad-ins.com>
2023-07-18 10:59:32 +0700

    fix: isPageFromService = false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   Module: "FOU",
   isCore: true,
   SpinnerOnHttpPost: false,
-  isPageFromService: true,
+  isPageFromService: false,
 
   LosURL: 'https://r3app-server.ad-ins.com/LOS_DEV',
   FoundationR3Url: 'https://r3app-server.ad-ins.com/FOUNDATION_DEV',
