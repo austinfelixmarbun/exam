7e7c79f501bd04455944d1628bf80ea704d7ae9b
vinsencius.aa <vinsencius.aa@ad-ins.com>
2023-08-11 14:58:19 +0700

    chore: add refprepaidpurposemodule

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -161,6 +161,18 @@ export const Full_ROUTES: Routes = [
         .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
     }
   },
+  {
+    path: 'prepaidpurpose',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'module',
+        remoteEntry: envi.arR3Web + '/remoteEntry.js',
+        exposedModule: './RefPrepaidPurposeModule'
+      })
+        .then(m => m.RefPrepaidPurposeModule)
+        .catch(e => import('app/error-page/error-page.module').then(m => m.ErrorPageModule))
+    }
+  },
   //#endregion
 
   //#region ARMNT
