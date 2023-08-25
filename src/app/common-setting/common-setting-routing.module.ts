690beb956cd8a956d483803ae38e25c4c471242d
Taqiyya Hanun <taqiyya.hanun@ad-ins.com>
2023-08-25 10:08:20 +0700

    routing general setting dan reason

--- src/app/common-setting/common-setting-routing.module.ts
+++ src/app/common-setting/common-setting-routing.module.ts
@@ -668,6 +668,30 @@ const routes: Routes = [
           title: 'Custom Reason  ',
           page: 'Reason'
         }
+      },
+      {
+        path: PathConstant.SELF_CUSTOM_CS_REASON_DETAIL,
+        component: ReasonAddEditComponent,
+        data: {
+          title: 'Reason Paging',
+          page: 'ReasonRegistration'
+        },
+      },
+      {
+        path: PathConstant.CUSTOM_CS_GEN_SETTING,
+        component: UcTemplateComponent,
+        data: {
+          title: 'General Setting Maintenance Paging',
+          page: 'Generalsetting'
+        },
+      },
+      {
+        path: PathConstant.CUSTOM_CS_GEN_SETTING_DETAIL,
+        component: UcTemplateComponent,
+        data: {
+          title: 'General Setting Maintenance Add Edit',
+          page: 'GeneralSettingEdit'
+        },
       }
     ]
   }
