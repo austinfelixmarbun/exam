8266d37aec07eae9094bcea691519396a4fa3c7f
teresa octaviani <teresa.octaviani@ad-ins.com>
2023-09-14 09:12:18 +0700

    Self Custom FOU : Vendor : Credit Insurance Group

--- src/app/vendor/component/vendor/self-custom-vendor-paging/self-custom-vendor-paging.component.ts
+++ src/app/vendor/component/vendor/self-custom-vendor-paging/self-custom-vendor-paging.component.ts
@@ -86,6 +86,9 @@ export class SelfCustomVendorPagingComponent implements OnInit, OnDestroy {
         case CommonConstant.SURVEYOR_BRANCH:
           this.pageName = 'SurveyorBranchGroupPaging'
           break;
+        case CommonConstant.CRD_INSCO_BRANCH:
+          this.pageName = 'CreditInsuranceBranchGroupPaging'
+          break;
       }
     }
     else if (this.Type == "Default") {
