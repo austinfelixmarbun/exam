d0c7067119f8006c257494fd4c2e38734131a646
teresa octaviani <teresa.octaviani@ad-ins.com>
2023-09-14 15:55:02 +0700

    Self Custom FOU : Vendor : Fixing Supplier HO & Holding

--- src/app/vendor/self-custom-vendor-holding-add-edit/self-custom-vendor-holding-add-edit.component.ts
+++ src/app/vendor/self-custom-vendor-holding-add-edit/self-custom-vendor-holding-add-edit.component.ts
@@ -108,21 +108,7 @@ export class SelfCustomVendorHoldingAddEditComponent implements OnInit {
             MrIdTypeCode: this.VendorId == 0 || res.length == 0? this.itemIdType[0].Key : this.MrIdTypeCode
           })
 
-          this.Form.controls.IdNo.clearValidators();
-    
-          if(this.Form.controls.MrIdTypeCode.value == CommonConstant.MrIdTypeCodeEKTP)
-          {
-            this.Form.controls.IdNo.setValidators([Validators.required, Validators.pattern("^[0-9]+$"), Validators.minLength(16), Validators.maxLength(16)])
-            this.Form.controls.IdNo.updateValueAndValidity();
-            return
-          }
-        
-          if(this.Form.controls.MrVendorTypeCode.value == 'P')
-          {
-            this.Form.controls.IdNo.setValidators([Validators.required])
-          }
-        
-          this.Form.controls.IdNo.updateValueAndValidity();
+          this.setValidatorIdNo();
       });
 
       if (!this.VatForPersonal){
@@ -139,6 +125,30 @@ export class SelfCustomVendorHoldingAddEditComponent implements OnInit {
         }
       }
     }
+
+    if (ev == "MrIdTypeCode")
+    {
+      this.setValidatorIdNo();
+    }
+  }
+
+  setValidatorIdNo()
+  {
+    this.Form.controls.IdNo.clearValidators();
+    
+    if(this.Form.controls.MrIdTypeCode.value == CommonConstant.MrIdTypeCodeEKTP)
+    {
+      this.Form.controls.IdNo.setValidators([Validators.required, Validators.pattern("^[0-9]+$"), Validators.minLength(16), Validators.maxLength(16)])
+      this.Form.controls.IdNo.updateValueAndValidity();
+      return
+    }
+  
+    if(this.Form.controls.MrVendorTypeCode.value == 'P')
+    {
+      this.Form.controls.IdNo.setValidators([Validators.required, Validators.pattern("^[0-9]+$")])
+    }
+  
+    this.Form.controls.IdNo.updateValueAndValidity();
   }
 
 }
