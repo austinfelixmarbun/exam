a837ff04f4e169ea02eb4b36434a3586f6108714
Andre Sucipto <andre.sucipto@ad-ins.com>
2023-09-15 19:14:34 +0700

    uc temp: vendor credit insurance branch

--- src/app/vendor/component/vendor/self-custom-vendor-detail/self-custom-vendor-detail.component.ts
+++ src/app/vendor/component/vendor/self-custom-vendor-detail/self-custom-vendor-detail.component.ts
@@ -91,6 +91,9 @@ export class SelfCustomVendorDetailComponent implements OnInit, OnDestroy {
       else if (this.MrVendorCategoryCode == CommonConstant.ASSET_INSCO_BRANCH){
         this.pageName = 'VendorInsuranceDetail'
       }
+      else if (this.MrVendorCategoryCode == CommonConstant.CRD_INSCO_BRANCH){
+        this.pageName = 'VendorCreditInsuranceDetail'
+      }
       else if (this.MrVendorCategoryCode == CommonConstant.AGENCY_COMPANY){
         this.pageName = 'VendorAgencyCompanyDetail'
       }
