6b6f326e66084cc62827adbedc632f97fe7c5791
Handy Satria <handy.sb@ad-ins.com>
2023-09-19 08:46:52 +0700

    FOUINT-004, Add Cust Coy Industry Info

---
+++ src/app/customer/customer-company/self-custom-customer-company/self-custom-cust-company-industry-info/self-custom-cust-company-industry-info.component.ts
@@ -0,0 +1,129 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CustPersonalFinDataObj } from 'app/shared/model/cust-personal-fin-data-obj.model';
+import { UrlConstantNew } from 'app/shared/constant/URLConstantNew';
+import { CommonConstant } from 'app/shared/constant/CommonConstant';
+import { FormBuilder, Validators } from '@angular/forms';
+import { InputLookupObj } from 'app/shared/model/input-lookup-obj.model';
+import { AdInsHelper } from 'app/shared/AdInsHelper';
+import { Router } from '@angular/router';
+import { CustCompanyObj } from 'app/shared/model/cust-company-obj.model';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
+import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
+import { DatePipe } from '@angular/common';
+import { ResCustCompanyIndustryInfoObj } from 'app/shared/model/res-cust-company-industry-info-obj.model';
+import { ReqAddEditCustCompanyIndustryInfoObj } from 'app/shared/model/req-add-edit-cust-company-industry-info-obj.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-self-custom-cust-company-industry-info',
+  templateUrl: './self-custom-cust-company-industry-info.component.html',
+  styleUrls: []
+})
+export class SelfCustomCustCompanyIndustryInfo implements OnInit {
+  ListCustIndustryInfo: Array<ResCustCompanyIndustryInfoObj> = [];
+  ReqCustIndustryInfoObj = new ReqAddEditCustCompanyIndustryInfoObj;
+  industryInfo: ResCustCompanyIndustryInfoObj;
+  custPersonalId: number;
+  mrMaritalStatCode: string;
+  currentModal: any;
+  lookUpObj: InputLookupObj;
+  mode : string;
+  
+  @Input() CustId: number;
+  @Input() CustNo: string;
+  @ViewChild('ModalIndustryList') ModalIndustryList;
+
+  IndustryInfoForm = this.fb.group({
+    BusinessStartDate: ['', [Validators.required]],
+    Notes: [''],
+    IsMain: [false],
+    RefIndustryTypeCode: [],
+    RefIndustryTypeName: [],
+    RowVersion: ['']
+  });
+  constructor(
+      private http: HttpClient,
+      private router: Router,
+      private toastr: NGXToastrService,
+      private fb: FormBuilder,
+      private modalService: NgbModal, 
+      private UrlConstantNew: UrlConstantNew,
+  ) { }
+
+  async ngOnInit(): Promise<void> { 
+    this.lookUpObj = new InputLookupObj(this.UrlConstantNew);
+    this.lookUpObj.urlJson = "./assets/lookup/lookupIndustryType.json";
+    this.lookUpObj.pagingJson = "./assets/lookup/lookupIndustryType.json";
+    this.lookUpObj.genericJson = "./assets/lookup/lookupIndustryType.json";
+    this.lookUpObj.isReady = true
+    await this.getListCustIndustryInfo();
+  }
+
+  
+  async getListCustIndustryInfo() {
+    this.ListCustIndustryInfo = [];
+    await this.http.post(this.UrlConstantNew.GetListCustCompanyIndustryInfoByCustId, { Id: this.CustId  }).toPromise().then((response: ResCustCompanyIndustryInfoObj) => {
+      this.ListCustIndustryInfo = response['ListCustCompanyIndustryInfoObj']; 
+    })    
+  }
+
+  showModalIndustryInfo(i: number) {
+      this.getSingleIndustryInfo(i);
+      this.currentModal = this.modalService.open(this.ModalIndustryList, { ariaLabelledBy: 'modal-basic-title', backdrop: 'static', keyboard: false });
+  }
+
+  getSingleIndustryInfo (i : number){
+      this.industryInfo = this.ListCustIndustryInfo[i];
+      var datePipe = new DatePipe("en-US");
+      if (!this.industryInfo) {
+        this.mode = 'add';
+        this.industryInfo = new ResCustCompanyIndustryInfoObj();
+        this.lookUpObj.nameSelect = ''
+        this.lookUpObj.jsonSelect = {IndustryTypeName : ''}
+      }else{
+        this.mode = 'edit'
+        this.lookUpObj.nameSelect = this.industryInfo.RefIndustryTypeName
+        this.lookUpObj.jsonSelect = {IndustryTypeName : this.industryInfo.RefIndustryTypeName}
+      }
+      this.IndustryInfoForm.patchValue({
+        BusinessStartDate: datePipe.transform(this.industryInfo.BusinessStartDate, 'yyyy-MM-dd'),
+        Notes: this.industryInfo.Notes,
+        IsMain: this.industryInfo.IsMain,
+        RefIndustryTypeCode: this.industryInfo.RefIndustryTypeCode
+      });
+  }
+
+  getLookUp(ev){
+    this.IndustryInfoForm.patchValue({
+      RefIndustryTypeCode: ev.IndustryTypeCode,
+      RefIndustryTypeName: ev.IndustryTypeName
+    })
+  }
+
+  async SaveIndustryInfo(){  
+   
+      this.ReqCustIndustryInfoObj = new ReqAddEditCustCompanyIndustryInfoObj
+      this.ReqCustIndustryInfoObj.CustNo = this.CustNo,
+      this.ReqCustIndustryInfoObj.RefIndustryTypeCode =this.IndustryInfoForm.controls['RefIndustryTypeCode'].value,
+      this.ReqCustIndustryInfoObj.BusinessStartDate= this.IndustryInfoForm.controls['BusinessStartDate'].value,
+      this.ReqCustIndustryInfoObj.IsMain= this.IndustryInfoForm.controls['IsMain'].value
+      this.ReqCustIndustryInfoObj.Notes= this.IndustryInfoForm.controls['Notes'].value
+      if(this.mode ==='edit'){
+        this.ReqCustIndustryInfoObj.CustCompanyIndustryInfoId = this.industryInfo.CustCompanyIndustryInfoId;
+        this.ReqCustIndustryInfoObj.RowVersion = this.industryInfo.RowVersion;
+      }
+      await this.http.post(this.UrlConstantNew.AddEditCustCompanyIndustryInfo, this.ReqCustIndustryInfoObj, AdInsConstant.SpinnerOptions).toPromise().then(
+        (response) => {
+          this.toastr.successMessage(response["Message"]);
+        }
+      );
+      this.currentModal.close("Success");
+      this.getListCustIndustryInfo();
+      AdInsHelper.RedirectUrl(this.router,["/BREAD/CustomerComponyV2"],{ IdCust : this.CustId});   
+    }
+  
+
+
+
+}
