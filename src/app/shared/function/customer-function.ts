33fce50484f858ab044cc0d8bb01085c275a31a8
Richard Suprayogi <richard.suprayogi@ad-ins.com>
2023-10-23 21:35:14 +0700

    FOUINT-013, FOUINT-004 - fix & rapikan industry

--- src/app/shared/function/customer-function.ts
+++ src/app/shared/function/customer-function.ts
@@ -1967,8 +1967,8 @@ export function addBouwheerCompany(parentForm: any, dicts: Record<string, any>, 
     async (response) => {
       resSave = response;
 
-      if (Array.isArray(forUpload) && forUpload.length > 0) {
-
+      if (Array.isArray(forUpload) && forUpload.length > 0 && forUpload.every(obj => obj.ByteBase64?.trim() && obj.DocUploadName?.trim())) 
+      {
         let reqFileUpl = {
           BouwheerId: resSave["Id"] ,
           uploadIndustryDocs: forUpload
@@ -1984,63 +1984,13 @@ export function addBouwheerCompany(parentForm: any, dicts: Record<string, any>, 
           "reqObj"
           )
         .then(() => {
-          // Berhasil diunggah
         })
         .catch((error) => {
-          // Gagal unggah, error dapat digunakan untuk menampilkan pesan kesalahan
         });
         await router.navigate(['/Customer/SelfCustom/Bouwheer/Paging']);
-
-        // let reqObj = {
-        //   BouwheerId: resSave["Id"] ,
-        //   uploadIndustryDocs: forUpload
-        // };
-  
-        // let urlUpload = environment.FoundationR3Url + "/v1/BouwheerCompanyIndustryInfo/UploadBouwheerCompanyIndustryDoc";
-        // // await this.uploadDocFileMultipartForGeneralPurpose(reqObj, resSave["Message"], toastr, cookieService, urlUpload, router)
-        
-        // try {
-        //   if (environment.SpinnerOnHttpPost) this.spinner.show();
-      
-        //   const formData: any = new FormData();
-        //   formData.append('reqObj', JSON.stringify(reqObj));
-      
-        //   const xhr = new XMLHttpRequest();
-      
-        //   const xhrPromise = new Promise<void>((resolve, reject) => {
-        //     xhr.onreadystatechange = evnt => {
-        //       if (xhr.readyState === 4) {
-        //         if (xhr.status === 200 || xhr.status === 201) {
-        //           resolve();
-        //         } else {
-        //           reject(new Error('Upload Failed !'));
-        //         }
-        //       }
-        //     };
-      
-        //     xhr.onerror = evnt => {
-        //       reject(new Error('Upload Failed !'));
-        //     };
-      
-        //     xhr.open('POST', urlUpload, true);
-        //     const value = cookieService.get('XSRF-TOKEN');
-        //     const token = DecryptString(value, environment.ChipperKeyCookie);
-        //     xhr.setRequestHeader('AdInsKey', `${token}`);
-        //     xhr.send(formData);
-        //   });
-      
-        //   await xhrPromise; // Tunggu sampai permintaan XHR selesai
-      
-        // } catch (error) {
-        //   toastr.errorMessage(error.message || 'An error occurred during upload.');
-        // } finally {
-        //   if (environment.SpinnerOnHttpPost) this.spinner.hide();
-        //   toastr.successMessage(resSave["Message"])
-        //   await router.navigate(['/Customer/SelfCustom/Bouwheer/Paging']);
-        // }
       }
       else{
-        toastr.successMessage(resSave["Message"])
+        // toastr.successMessage(resSave["Message"])
         await router.navigate(['/Customer/SelfCustom/Bouwheer/Paging']);
       }
     });
