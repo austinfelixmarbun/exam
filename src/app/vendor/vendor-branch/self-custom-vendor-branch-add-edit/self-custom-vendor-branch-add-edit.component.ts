e8445633776874032f827c3630a4fae9d3f756c4
Richard Suprayogi <richard.suprayogi@ad-ins.com>
2023-11-15 15:33:52 +0700

    SITCSULRTHREE-719: fix personal agency self custom

--- src/app/vendor/vendor-branch/self-custom-vendor-branch-add-edit/self-custom-vendor-branch-add-edit.component.ts
+++ src/app/vendor/vendor-branch/self-custom-vendor-branch-add-edit/self-custom-vendor-branch-add-edit.component.ts
@@ -79,7 +79,8 @@ export class SelfCustomVendorBranchAddEditComponent implements OnInit {
       else if (this.MrVendorCategoryCode == CommonConstant.ASSET_INSCO_BRANCH || this.MrVendorCategoryCode == CommonConstant.LIFE_INSCO_BRANCH || this.MrVendorCategoryCode == CommonConstant.SURVEYOR_BRANCH ||
         this.MrVendorCategoryCode == CommonConstant.AUCTION_COMPANY || this.MrVendorCategoryCode == CommonConstant.COLL_COMPANY || this.MrVendorCategoryCode == CommonConstant.AGENCY_COMPANY || this.MrVendorCategoryCode == CommonConstant.NOTARY_COMPANY
         || this.MrVendorCategoryCode == CommonConstant.NOTARY_PERSONAL
-        || this.MrVendorCategoryCode == CommonConstant.CRD_INSCO_BRANCH)
+        || this.MrVendorCategoryCode == CommonConstant.CRD_INSCO_BRANCH
+        || this.MrVendorCategoryCode == CommonConstant.AGENCY_PERSONAL)
       {
         this.pageName = "VendorbranchregistrationV2"
       }
