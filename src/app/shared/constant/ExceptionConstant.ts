4b4b8e7e70137d974081cf3c60a0e4dd9a3d7f49
teresa octaviani <teresa.octaviani@ad-ins.com>
2023-12-01 18:32:10 +0700

    fix validasi working hour from kosong ketika ada working hour to

--- src/app/shared/constant/ExceptionConstant.ts
+++ src/app/shared/constant/ExceptionConstant.ts
@@ -11,6 +11,7 @@ export class ExceptionConstant {
     public static PLEASE_COMPLETE_LEGAL_ADDRESS = "Please complete Legal Address First";
     public static STATEMENT_WITH_SAME_MONTH_AND_YEAR = "Cannot Input Statement With The Same Month and Year";
     public static WORKING_HOUR_CHECKING = 'Working Hour {0} {1} {2} Working Hour {3} {4}';
+    public static WORKING_HOUR_FROM_CAN_NOT_BE_EMPTY_WHEN_THERE_IS_WORKING_HOUR_TO = 'Working Hour {0} {1} can not be empty when there is Working Hour {2} {3}';
     public static TOTAL_SHARE_LEFT = "Total Share left is ";
     public static TOTAL_SHARE_MUST_100 = "Total Share % must be 100%";
     public static TOTAL_SHARE_CAN_NOT_100 = "Total Share can not more than 100%";
