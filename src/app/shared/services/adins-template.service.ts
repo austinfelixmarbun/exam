c94edeb36259172c63013fa5eb02620a55ae21d3
Muhammad Syafiudin <muhammad.ss@ad-ins.com>
2023-12-08 13:08:45 +0700

    Fix versi uc template baru

--- src/app/shared/services/adins-template.service.ts
+++ src/app/shared/services/adins-template.service.ts
@@ -42,5 +42,6 @@ export class AdinsTemplateService extends UcTemplateService {
     this.moment = _moment;
     this.module = Module;
     this.navConstant = NavigationConstant
+    this.baseUrl = envi.FoundationR3Web;
   }
 }
