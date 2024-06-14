bdf23e715f1e867773392c59b97ef4adc662c09d
kristy <kristy.tiffani@ad-ins.com>
2024-06-14 10:53:43 +0700

    add custom pattern for no whitespace in input code menu verification and journal

--- src/app/survey/survey-routing.module.ts
+++ src/app/survey/survey-routing.module.ts
@@ -187,7 +187,7 @@ const routes: Routes = [
         component: UcTemplateComponent,
         data:{
           title: 'Survey Result Review Detail',
-          page: 'SurveyResultReviewDetail'
+          page: 'Surveyresultreviewdetail'
         }
       },
       {
