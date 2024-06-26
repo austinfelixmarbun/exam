281c5c93b3231816e73d2f5f5802a27ca53ec0da
stephen.hermanto <stephen.hermanto@ad-ins.com>
2024-06-26 11:04:15 +0700

    ganti isPageFromService di environment jadi false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
   Module: "FOU",
   isCore: true,
   SpinnerOnHttpPost: false,
-  isPageFromService: true,
+  isPageFromService: false,
   useSafeUrl: true,
 
   losUrl: 'https://r3app-server.ad-ins.com/LOS_DEV',
