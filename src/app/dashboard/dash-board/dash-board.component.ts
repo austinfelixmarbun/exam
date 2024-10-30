12c456da49dbf9c7ea34061dc7b045427638e739
shafiq.affandi <muhammad.sa@ad-ins.com>
2024-10-30 16:46:33 +0700

    remove things to do

--- src/app/dashboard/dash-board/dash-board.component.ts
+++ src/app/dashboard/dash-board/dash-board.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AdInsHelper } from 'app/shared/AdInsHelper';
-import { CommonConstant } from 'app/shared/constant/CommonConstant';
-import { URLConstant } from 'app/shared/constant/URLConstant';
-import { ThingsToDoIntegrationV2Obj, UcThingsToDoObj } from 'app/shared/model/library/uc-things-to-do-obj.model';
-import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'app-dash-board',
@@ -11,113 +6,11 @@ import { CookieService } from 'ngx-cookie';
   styleUrls: ['./dash-board.component.scss']
 })
 export class DashBoardComponent implements OnInit {
-  Item: UcThingsToDoObj = new UcThingsToDoObj();
 
-  username: string;
-  url: string;
-  officeCode: string;
-  roleCode: string;
 
-  constructor(private cookieService: CookieService) { }
+  constructor() { }
 
   ngOnInit() {
-    // this.Item = {Url : AdInsConstant.GetThingsToDoByRole, Module : "FOU"};
-
-    // let UserAccess = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
-    // this.http.post<ResDashboardObj>(AdInsConstant.GetDashboardAccessToken, {UserName: UserAccess[CommonConstant.USER_NAME]}).subscribe(
-    //     (response) => {
-    //       if (response.dashboardUrl != null && response.dashboardUrl != "") {
-    //         this.urlLink = response.dashboardUrl;
-    //         this.isReady = true;
-    //       }
-    //     }
-    // );
-    let context = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
-    this.username = context[CommonConstant.USER_NAME];
-    this.url = URLConstant.env.DashboardURL;
-    this.officeCode = context[CommonConstant.OFFICE_CODE];
-    this.roleCode = context[CommonConstant.ROLE_CODE];
-    this.Item.Url = URLConstant.GetThingsToDoByRoleV2;
-    this.Item.RequestObj.ModuleCode = CommonConstant.MODULE_FOU;
-
-    let integrationObj;
-    let integrationObj2;
-
-    integrationObj = new ThingsToDoIntegrationV2Obj();
-    integrationObj.BaseUrl = URLConstant.GetThingsToDoCamunda;
-    integrationObj.ApiPath = "";
-    integrationObj.RequestObj.OfficeCode = "";
-    integrationObj.RequestObj.UserName = this.username;
-
-    integrationObj2 = new ThingsToDoIntegrationV2Obj();
-    integrationObj2.BaseUrl = URLConstant.GetListApvTaskListByUsernameAndRoleCodeForThingsToDo;
-    integrationObj2.ApiPath = "";
-    integrationObj2.RequestObj.OfficeCode = "";
-    integrationObj2.RequestObj.UserName = this.username;
-    integrationObj2.RequestObj.RoleCode = this.roleCode;
-    this.Item.RequestObj.IntegrationObj.push(integrationObj2);
-    
-    if (context[CommonConstant.MR_OFFICE_TYPE_CODE] == CommonConstant.HeadOffice) {
-      integrationObj.RequestObj.OfficeRoleCodes = [this.roleCode, this.roleCode + "-" + this.officeCode, this.officeCode];
-    } else {
-      integrationObj.RequestObj.OfficeRoleCodes = [this.roleCode + "-" + this.officeCode, this.officeCode];
-    }
-
-    this.Item.RequestObj.IntegrationObj.push(integrationObj);
-  }
-  
-  showMessage(message: any) {
-  }
-
-  public data: any[] = [
-    {
-      text: 'Furniture', items: [
-        { text: 'Tables & Chairs' },
-        { text: 'Sofas' },
-        { text: 'Occasional Furniture' }
-      ]
-    },
-    {
-      text: 'Decor', items: [
-        { text: 'Bed Linen' },
-        { text: 'Curtains & Blinds' },
-        { text: 'Carpets' }
-      ]
-    }
-  ];
-
-  public items: any[] = [{ text: 'Remove', icon: 'close' }];
-
-  private contextItem: any;
-
-  public onNodeClick(e: any): void {
-    if (e.type == 'contextmenu') {
-      const originalEvent = e.originalEvent;
-
-      originalEvent.preventDefault();
-
-      this.contextItem = e.item.dataItem;
-
-    }
-  }
-
-  public onSelect({ item }): void {
-    if (item.text == 'Remove') {
-      this.removeItem(this.contextItem, this.data);
-    }
-  }
-
-  private removeItem(dataItem: any, items: any[]): void {
-    const index = items.indexOf(dataItem);
-    if (index >= 0) {
-      items.splice(index, 1);
-    } else {
-      items.forEach(item => {
-        if (item.items) {
-          this.removeItem(dataItem, item.items);
-        }
-      });
-    }
   }
 
 }
